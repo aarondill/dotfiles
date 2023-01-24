855e557c3731b11ae3f63ec5199db4edd2d9991c
aarondill <aaronsacks2006@gmail.com>
2023-01-23 22:38:31 -0600

    Initial commit

---
+++ vscode/dot_prettierrc.js
@@ -0,0 +1,14 @@
+// ~/vscode/.prettierrc.js
+module.exports = {
+	arrowParens: 'avoid',
+	bracketSameLine: true,
+	bracketSpacing: true,
+	embeddedLanguageFormatting: 'auto',
+	quoteProps: 'as-needed',
+	endOfLine: 'lf',
+	semi: true,
+	singleAttributePerLine: false,
+	singleQuote: false,
+	trailingComma: 'es5',
+	useTabs: true,
+};
