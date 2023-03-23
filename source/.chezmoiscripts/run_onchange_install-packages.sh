d157ddd36360d2ef93a3118a63991f3f8fc1965f
aarondill <aaronsacks2006@gmail.com>
2023-03-22 23:42:13 -0500

    download bitwarden
    Former-commit-id: 39e56af0c3d0002fc44839e0c1a55b7dfe5bde4e
    Former-commit-id: 8fd73f189c566c66676c6c7602d112b4dde49485
    Former-commit-id: 21d61ae91462a0d511a7651e791d50495406afea

--- source/.chezmoiscripts/run_onchange_install-packages.sh
+++ source/.chezmoiscripts/run_onchange_install-packages.sh
@@ -15,14 +15,17 @@ function quiet_apt() {
   sudo apt-get -qq "$@" >/dev/null
 }
 function install_if_available_apt() {
+  declare -i exit=0
   local package
   for package; do
     if is_available_apt "$package"; then
-      quiet_apt install -- "$package"
+      # If failed, set exit to failure code
+      quiet_apt install -- "$package" || exit=$?
     else
       echo "It appears that $package is not available from the apt repositories."
     fi
   done
+  return $exit
 }
 function is_available_apt() {
   test -n "$(apt-cache show -- "$1" 2>/dev/null)"
@@ -33,46 +36,54 @@ function is_accessible_cmd() {
 function log() {
   printf '%s\n' "$@"
 }
+function err() {
+  printf '%s\n' "$@" >&2
+}
 read -rep "Would you like to install some things? (yes) " confirmation
 if [[ -z "$confirmation" || "${confirmation,,}" =~ ^\s*y(es)?\s*$ ]]; then
   # Get user password
   sudo -v
-  log 'Installing apt packages...'
   # TODO, make this count which are available and install at once
-  install_if_available_apt age anacron apt apt-clone autopoint bat command-not-found \
-    curl dconf-editor duf flatpak fwts gh gimp git gnome-shell-extension-manager \
-    golang-go gparted grep gucharmap httpie ifupdown inotify-tools \
-    less luckybackup make neofetch neovim net-tools okular openvpn \
-    python3-neovim qtqr rsync shfmt tlp trash-cli tree util-linux xclip httpie \
-    xdg-utils zeal zip zoxide gnome-software gnome-software-plugin-flatpak
+  (
+    set -e
+    log 'Installing apt packages...'
+    install_if_available_apt age anacron apt apt-clone autopoint bat command-not-found \
+      curl dconf-editor duf flatpak fwts gh gimp git gnome-shell-extension-manager \
+      golang-go gparted grep gucharmap httpie ifupdown inotify-tools \
+      less luckybackup make neofetch neovim net-tools okular openvpn \
+      python3-neovim qtqr rsync shfmt tlp trash-cli tree util-linux xclip httpie \
+      xdg-utils zeal zip zoxide gnome-software gnome-software-plugin-flatpak
+    log 'success!'
+  ) || err 'something went wrong installing apt packages!'
   # Maintain sudo after long install
   sudo -v
   if ! is_accessible_cmd pnpm; then
     log 'installing nodejs and pnpm...'
-    install_if_available_apt npm
-    if which npm >/dev/null; then
+    (
+      set -e # stop immediately on error!
+      install_if_available_apt npm
+      which npm >/dev/null # exits if fails
       # Setup n
-      "$(which npm)" install -g n
+      "$(which npm)" install --silent -g n
       sudo mkdir -p /usr/local/n
       sudo chown -- "$(whoami)" /usr/local/n
-      n lts # installs node and npm
+      n --quiet lts # installs node and npm
 
       # Remove npm from apt
-      "$(which npm)" remove -g n
+      "$(which npm)" remove --silent -g n
       quiet_apt autoremove npm
 
       # Setup pnpm
       corepack enable
-      corepack prepare --activate pnpm@latest
-      pnpm i -g n
-    else
-      echo 'could not install npm from apt'
-    fi
+      corepack prepare pnpm@latest --activate >/dev/null
+      pnpm i --silent -g n
+      log 'Success!'
+    ) || err 'Something went wrong installing nodejs and pnpm!'
   fi
   remove_if_installed_apt gnome-characters
 
   # setup for custom ppas
-  quiet_apt install wget gpg curl
+  quiet_apt install gpg curl # should already be installed, but sanity check
 
   # Require custom ppas
   log 'installing spotify ppa'
@@ -80,13 +91,14 @@ if [[ -z "$confirmation" || "${confirmation,,}" =~ ^\s*y(es)?\s*$ ]]; then
   echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list >/dev/null
 
   log 'installing vscode ppa'
-  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor --yes -o /etc/apt/keyrings/packages.microsoft.gpg
+  curl -sS https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor --yes -o /etc/apt/keyrings/packages.microsoft.gpg
   echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list >/dev/null
 
   log 'installing google-chrome ppa'
-  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/google-chrome.gpg
+  curl -sS https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/google-chrome.gpg
   echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google.list >/dev/null
 
+  log 'Installing spotify-client, code, and google-chrome packages'
   quiet_apt update
   install_if_available_apt spotify-client code google-chrome-stable
   quiet_apt install -f
@@ -104,23 +116,51 @@ if [[ -z "$confirmation" || "${confirmation,,}" =~ ^\s*y(es)?\s*$ ]]; then
   # Install flatpaks, if flatpak is installed
   if is_accessible_cmd flatpak &>/dev/null; then
     # remove cheese if present
+    log 'Setting up flatpak'
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    remove_if_installed_apt cheese
-    flatpak install com.github.johnfactotum.Foliate \
-      com.github.tchx84.Flatseal \
-      com.valvesoftware.Steam \
-      io.mrarm.mcpelauncher \
-      org.gnome.Boxes \
-      org.gnome.Cheese \
-      org.libretro.RetroArch \
-      com.github.alainm23.planner
+    (
+      set -e
+      log 'Installing flatpak packages...'
+      remove_if_installed_apt cheese
+      flatpak install -y --noninteractive flathub com.github.johnfactotum.Foliate >/dev/null
+      flatpak install -y --noninteractive flathub com.github.tchx84.Flatseal >/dev/null
+      flatpak install -y --noninteractive flathub com.valvesoftware.Steam >/dev/null
+      flatpak install -y --noninteractive flathub io.mrarm.mcpelauncher >/dev/null
+      flatpak install -y --noninteractive flathub org.gnome.Boxes >/dev/null
+      flatpak install -y --noninteractive flathub org.gnome.Cheese >/dev/null
+      flatpak install -y --noninteractive flathub org.libretro.RetroArch >/dev/null
+      flatpak install -y --noninteractive flathub com.github.alainm23.planner >/dev/null
+      log 'Success!'
+    ) || err 'Something went wrong installing flatpak packages'
   else
     log "Flatpak is not installed, skipping flatpak installations."
   fi
   if ! is_accessible_cmd bitwarden &>/dev/null; then
-    log "The bitwarden appimage can be installed from their website and should be placed in /usr/local/bin/bitwarden"
+    (
+      set -e
+      log 'Installing bitwarden desktop...'
+      # Is an appimage
+      sudo curl -sSL 'https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=appimage' -o /usr/local/bin/bitwarden
+      sudo chmod +x /usr/local/bin/bitwarden
+      log 'Success!'
+    ) || err 'Something went wrong installing bitwarden desktop'
+  fi
+  if ! bw --version &>/dev/null; then
+    (
+      set -e
+      log 'Installing bitwarden CLI...'
+      # Is a zip file
+      temp_dir=$(mktemp --tmpdir -d 'config-install-XXXXXX')
+      sudo curl -sSL 'https://vault.bitwarden.com/download/?app=cli&platform=linux' -o "$temp_dir/bw.zip"
+      unzip "$temp_dir/bw.zip"
+      mv "$temp_dir/bw" /usr/local/bin/bw
+      rm -rf "$temp_dir"
+      sudo chmod +x /usr/local/bin/bw
+      log 'Success!'
+    ) || err 'Something went wrong installing bitwarden CLI'
   fi
 
+  # TODO, make this automatic!
   if ! is_accessible_cmd fzf &>/dev/null; then
     log "fzf should be downloaded from the git repo and placed in /usr/bin/fzf"
   fi
