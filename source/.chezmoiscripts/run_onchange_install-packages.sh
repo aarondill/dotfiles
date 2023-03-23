74398a49b58490f13a18f58c9c44f3beb07bdb5c
aarondill <aaronsacks2006@gmail.com>
2023-03-22 22:17:27 -0500

    install pnpm
    Former-commit-id: d9c0287256b625f9b7eb31d61044fc04f92a95b5
    Former-commit-id: 6692e2f5056da7cb416cf76855d707b69e199c4c
    Former-commit-id: 519d271bbb8588997c1416c485d3c36f24e01a63

--- source/.chezmoiscripts/run_onchange_install-packages.sh
+++ source/.chezmoiscripts/run_onchange_install-packages.sh
@@ -46,17 +46,18 @@ if [[ -z "$confirmation" || "${confirmation,,}" =~ ^\s*y(es)?\s*$ ]]; then
     xdg-utils zeal zip zoxide gnome-software gnome-software-plugin-flatpak
   # Maintain sudo after long install
   sudo -v
-  if ! is_accessible_cmd n; then
+  if ! is_accessible_cmd pnpm; then
     log 'installing nodejs. After getting node setup using `n`, run `sudo apt remove nodejs`'
     install_if_available_apt npm
     npm install -g n
     sudo mkdir -p /usr/local/n
     sudo chown -- "$(whoami)" /usr/local/n
     n lts # installs node and npm
+    corepack enable
     corepack prepare --activate pnpm@latest
     pnpm i -g n
     npm remove -g n
-    quiet_apt remove npm
+    quiet_apt autoremove npm
   fi
   remove_if_installed_apt gnome-characters
 
