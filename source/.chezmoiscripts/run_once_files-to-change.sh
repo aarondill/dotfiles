7537d5175a3e6e55c588027b180c09be0f93019b
aarondill <aaronsacks2006@gmail.com>
2023-03-27 21:23:08 -0500

    add information message about manually changed locations
    Former-commit-id: 203aeb053d1647d3216fc176a873bfc1ca9cbe59
    Former-commit-id: e4723376cc353a1d3e86728ca50e9ebe2c4a6e00
    Former-commit-id: c0858ff2547ba2ae758a755e9cd51beb26d2f5c3

---
+++ source/.chezmoiscripts/run_once_files-to-change.sh
@@ -0,0 +1,4 @@
+function log(){ printf '%s\n' "$@"; }
+log 'These files must be changed if the hard-drive uuids have changed'
+log "/etc/default/grub, /etc/fstab, /etc/initramfs-tools/conf.d/resume"
+
