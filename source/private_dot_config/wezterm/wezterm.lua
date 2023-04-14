42a0d15177cc29c441eef5afe97124b75ded4a3b
aarondill <aaronsacks2006@gmail.com>
2023-04-14 03:55:18 -0500

    Update source/.config/wezterm/wezterm.lua
    Former-commit-id: 1fb6c677b4a57a2421e0e8c3a22da9796d48c2c7
    Former-commit-id: 02a96b280c20a144db7ff7e3c9d53c9ad88742a4
    Former-commit-id: 9e34b3cb05756f651d3ce37ebc6dd2734d37c5c1

--- source/private_dot_config/wezterm/wezterm.lua
+++ source/private_dot_config/wezterm/wezterm.lua
@@ -14,6 +14,7 @@ end
 
 -- For example, changing the color scheme:
 -- config.color_scheme = "AdventureTime"
+config.color_scheme = "Tango (terminal.sexy)"
 
 -- and finally, return the configuration to wezterm
 return config
