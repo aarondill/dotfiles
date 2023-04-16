bd05644c156c9e5e113b1e28c074eff660807511
aarondill <aaronsacks2006@gmail.com>
2023-04-16 17:45:09 -0500

    Add source/.config/wezterm/plugins/functional_settings.lua Add source/.config/wezterm/plugins/term.lua Add source/.config/wezterm/plugins/theme.lua Add source/.config/wezterm/plugins/toggle-transparency.lua
    Former-commit-id: fca9b055fc40f2fb3cbf9c5377614aa85fec95ed
    Former-commit-id: df30a75514d00b945c459fee17833c848d3bc8c7
    Former-commit-id: 2de3b2fd46a0a0eb88da0009fa6cfb7267750b70

---
+++ source/private_dot_config/exact_wezterm/exact_plugins/term.lua
@@ -0,0 +1,12 @@
+local util = require("util")
+local M = {}
+
+M.apply = function(config)
+	local root_terminfo = "/lib/terminfo/w/wezterm"
+	local local_terminfo = os.getenv("HOME") .. "/.terminfo/w/wezterm"
+	if util.file_exists(root_terminfo) or util.file_exists(local_terminfo) then
+		config.term = "wezterm"
+	end
+end
+
+return M
