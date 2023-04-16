9468904871c6499e933c23b43875cc5d4dc10666
aarondill <aaronsacks2006@gmail.com>
2023-04-16 16:06:30 -0500

    Add source/.chezmoiscripts/set_splash.sh Remove source/.root/etc/initramfs-tools/conf.d/splash
    Former-commit-id: 0857f9365f1eeb8be4e84fb334a8a18a733d5d00
    Former-commit-id: 41fa906113aa448fd9ceddac13677b39ce558d49
    Former-commit-id: 2e23f3e7a2bef961121f7e0fd4ea5834f190cc91

---
+++ source/.chezmoiscripts/run_once_set_splash.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Install vortex-ubuntu-plymouth-theme splash screen
+
+tempdir=$(mktemp -d) && (
+	# Stop on error and remove temporary dir
+	set -e
+	trap 'rm -rf "$tempdir"' EXIT
+
+	# Clone and cd into repo
+	git clone 'https://github.com/emanuele-scarsella/vortex-ubuntu-plymouth-theme' "$tempdir" --depth 1
+	cd "$tempdir"
+
+	# Create conf.d to show the splash on boot
+	if [[ -f /etc/initramfs-tools/conf.d/splash ]]; then
+		printf 'Overriding /etc/initramfs-tools/conf.d/splash'
+		sleep 3 # Allow user to halt
+	fi
+	printf 'FRAMEBUFFER=y' | sudo tee /etc/initramfs-tools/conf.d/splash
+
+	# Run install script
+	sudo chmod +x "$tempdir/install"
+	sudo "$tempdir/install"
+
+)
