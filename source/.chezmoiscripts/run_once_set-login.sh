0e71f81d5511abcbbfa7a082d8ce4104493f7c84
aarondill <aaronsacks2006@gmail.com>
2023-04-17 15:31:06 -0500

    set login screen
    Former-commit-id: 2c2c55e67110b3eae87735e5617c9c4561dbee01
    Former-commit-id: a452050ff389d3cabc74e1bd4f670bb552cfc09d
    Former-commit-id: 78bd42baf40a565b89ee3837ef6a087bbea84496

---
+++ source/.chezmoiscripts/run_once_set-login.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Install ubuntu-gdm-set-background
+set -e
+function log() { printf '%s\n' "$@"; }
+function success() { printf "$(tput setaf 2)%s$(tput sgr0)\n" "Success!"; }
+
+log "Installing ubuntu-gdm-set-background"
+OWNER="aarondill"
+FILE="ubuntu-gdm-set-background"
+DESTINATION="/usr/local/bin/$FILE"
+curl -SsfL "https://raw.githubusercontent.com/$OWNER/ubuntu-gdm-set-background/main/$FILE" |
+	tee "$DESTINATION" >/dev/null
+chmod +x "$DESTINATION"
+success
+
+log "Downloading sky_stars_moon_144720_1600x900.jpg"
+# Download image file
+TEMP_FILE="$(mktemp)"
+trap 'rm -f "$TEMP_FILE"' EXIT
+curl -SsfL "https://images.wallpaperscraft.com/image/single/sky_stars_moon_144720_1600x900.jpg" -o "$TEMP_FILE"
+success
+
+log 'Setting sky_stars_moon_144720_1600x900.jpg as current lock screen image'
+"$DESTINATION" --image "$TEMP_FILE"
+success
