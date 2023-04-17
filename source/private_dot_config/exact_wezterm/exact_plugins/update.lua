8674812091a316142b7e1c7ac19b1c36a17d423c
aarondill <aaronsacks2006@gmail.com>
2023-04-17 00:35:47 -0500

    Add source/.config/wezterm/README.md Add source/.config/wezterm/plugins/close.lua Add source/.config/wezterm/plugins/history.lua Add source/.config/wezterm/plugins/text.lua Add source/.config/wezterm/plugins/update.lua Add source/.config/wezterm/plugins/window.lua Add source/.config/wezterm/plugin.lua
    Former-commit-id: 4b9acadaa7ded5427d375c39fb82cf699ef508d0
    Former-commit-id: 2b80ba8be903b96d8c0f54e6e5be5f557ef27ce8
    Former-commit-id: 11f11b0cd2543e3a0d6fc9dec78df055cfaf5f76

---
+++ source/private_dot_config/exact_wezterm/exact_plugins/update.lua
@@ -0,0 +1,11 @@
+return {
+	-- bc cool (default)
+	automatically_reload_config = true,
+
+	-- Check for updates of wezterm (default)
+	check_for_updates = true,
+	-- Check three times a day (default 86400 - once a day)
+	check_for_updates_interval_seconds = math.floor((24 * 60 * 60) / 3),
+	-- Show me the updates! (default)
+	show_update_window = true,
+}
