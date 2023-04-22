0755204779e46ad8e3db4b07dd411785e453bf67
aarondill <aaronsacks2006@gmail.com>
2023-04-22 14:50:05 -0500

    dont echo input
    Former-commit-id: 56610163e75326b54f2a9c0728f03c9f51c581a0
    Former-commit-id: 85bd90d5cf7414edd5db029287c6dd9ed9d0c7ec
    Former-commit-id: 2af2d7f6176e0e0a68fd586e1338ebb4f67890fb

--- source/.chezmoiscripts/run_once_after_files-to-change.sh
+++ source/.chezmoiscripts/run_once_after_files-to-change.sh
@@ -4,4 +4,4 @@ log 'These files must be changed if the hard-drive uuids have changed'
 log "/etc/default/grub, /etc/fstab, /etc/initramfs-tools/conf.d/resume"
 # Wait for user input!
 log 'press any key to continue...'
-read -n1 -r </dev/tty
+read -n1 -rs </dev/tty
