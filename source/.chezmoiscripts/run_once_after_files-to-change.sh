bf300c31ff8ea3081d822be261d192ce51c3ba05
aarondill <aaronsacks2006@gmail.com>
2023-04-22 14:47:16 -0500

    ensure user understands
    Former-commit-id: 7633d90b270fc02f1b4b443a2a7ef5cd17f2ad27
    Former-commit-id: fa9e1094317d033821ed818bf114abbfdd603c25
    Former-commit-id: 1566e9fb85693b88106cecc0ff2abec61abc847e

--- source/.chezmoiscripts/run_once_after_files-to-change.sh
+++ source/.chezmoiscripts/run_once_after_files-to-change.sh
@@ -2,3 +2,6 @@
 function log() { printf '%s\n' "$@"; }
 log 'These files must be changed if the hard-drive uuids have changed'
 log "/etc/default/grub, /etc/fstab, /etc/initramfs-tools/conf.d/resume"
+# Wait for user input!
+log 'press any key to continue...'
+read -n1 -r </dev/tty
