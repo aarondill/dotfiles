0d0fa403b62a1fb5c154fbd18c915f62cfd94d5c
aarondill <aaronsacks2006@gmail.com>
2023-04-22 14:06:15 -0500

    extensions sync requires yarn...
    Former-commit-id: 3c7813f97e546cb06449d46d078c5b00aed15740
    Former-commit-id: 7db85d8377072fe6aec5c936f295cd761bef981a
    Former-commit-id: f014776569a0949ef09fa2cbccf6599ecb6db8e8

--- source/.chezmoiscripts/run_onchange_install-gnome-extension.sh
+++ source/.chezmoiscripts/run_onchange_install-gnome-extension.sh
@@ -8,9 +8,9 @@ if ! [ -f /usr/bin/gnome-session ]; then
 fi
 
 # Pick a package manager!
-NPM=$(which pnpm 2>/dev/null || which yarn 2>/dev/null || which npm 2>/dev/null)
+NPM=$(which yarn 2>/dev/null)
 if ! [ -x "${NPM}" ]; then
-  echo "npm, pnpm, or yarn are required to install extensions sync" >&2
+  echo "yarn is required to install extensions sync" >&2
   exit 1
 fi
 
@@ -23,7 +23,7 @@ trap 'rm -rf ${TEMP}' EXIT
 git clone https://github.com/oae/gnome-shell-extensions-sync.git "$TEMP"
 
 cd "$TEMP"
-"$NPM" install # Any of the three can do these two commands
+"$NPM" install
 "$NPM" run build
 mv -- "$PWD/dist" "$HOME/.local/share/gnome-shell/extensions/extensions-sync@elhan.io" # keep the dist directory as the final result
 
