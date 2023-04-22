ec9cf1da88349bf4742508b90377f6f9432fe7ab
aarondill <aaronsacks2006@gmail.com>
2023-04-22 13:40:14 -0500

    read from sync file
    Former-commit-id: 2a50f47cdd5c77b548fd17c1528fe4f0eaf221c9
    Former-commit-id: 78da7be29d250940cffe5f9ceebbcbf2c8488a67
    Former-commit-id: f05b030a3f53f4cb65a4b772ceb1c66804a55307

--- source/.chezmoiscripts/run_onchange_install-gnome-extension.sh
+++ source/.chezmoiscripts/run_onchange_install-gnome-extension.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 # Installs extension sync gnome extension
+set -e
 if ! [ -f /usr/bin/gnome-session ]; then
   # Gnome is not installed
+  echo "gnome is not installed, so skipping extension synchronization" >&2
   return 0
 fi
 
+# Install using gnome-shell-extension-installer
 curl -sSfL 'https://github.com/brunelli/gnome-shell-extension-installer/raw/master/gnome-shell-extension-installer' |
   bash /dev/stdin 1486
+
+# If sync file exists, read from it
+if [ -f "$HOME/.config/extensions-sync.json" ]; then
+  busctl --user call org.gnome.Shell /io/elhan/ExtensionsSync io.elhan.ExtensionsSync read # load from the file
+fi
