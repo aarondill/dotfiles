088322188c27aa78faa4145b0ac48c642156fe25
aarondill <aaronsacks2006@gmail.com>
2023-04-25 12:20:04 -0500

    move grub editor to own file
    Former-commit-id: d2699ba24183ff37ba4083e81a04a694c2b5169e
    Former-commit-id: da155e501def16ac3375f9a20a99a537b099a20b
    Former-commit-id: 11efba2dfcc92726093cb2667b5d9aa87c9d1512

--- source/.chezmoiscripts/install/run_onchange_after-packages.sh
+++ source/.chezmoiscripts/install/run_onchange_after-packages.sh
@@ -5,31 +5,6 @@ SOURCE_DIR=$(chezmoi source-path)
 . "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
 
 #region ## Actual Code {{{1
-#region ### Grub Editor {{{2
-function install_grub_editor() (
-  set -e # run in subshell
-  declare temp_dir version REPO=Thenujan-0/grub-editor
-  if ! { [ "$ARCH" = "x86_64" ] || [ "$ARCH" = "amd64" ]; }; then
-    err 'Only amd64 and x86_64 are supported at this time.'
-    return 1
-  fi
-
-  version=$(get_latest_version_github "$REPO") # v1.0.0
-
-  asset=grub-editor_${version#v}-1_amd64.deb # grub-editor_1.0.0-1_amd64.deb - no other files are available.
-  # Download the .deb
-  temp_dir=$(mktemp -d) &&
-    (
-      # In a subshell, so runs at end of block
-      trap 'rm -rf $temp_dir' EXIT
-      set -e
-      local destination=$temp_dir/grub-editor.deb
-      log_github_install "$REPO" "$version" "$asset" "$destination"
-      curl -sSL "https://github.com/$REPO/releases/download/$version/$asset" -o "$destination"
-      sudo apt install "$destination"
-    )
-)
-#endregion
 #region ### Fx {{{2
 function install_fx() {
   local ext short_arch asset
@@ -143,10 +118,6 @@ function install_java() (
 if ! confirm "Would you like to install some things?"; then
   err 'Aborting.'
   exit 0
-fi
-
-if is_accessible_cmd apt && ! is_available_apt grub-editor; then
-  log_and_run "Installing grub-editor" install_grub_editor
 fi
 
 log_and_run 'installing proprietary packages' install_proprietary_software spotify-client code google-chrome-stable
---
+++ source/.chezmoiscripts/install/run_onchange_after_grub-editor.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env bash
+# Source utils
+SOURCE_DIR=$(chezmoi source-path)
+# shellcheck source=../.utils.sh
+. "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
+
+function install_grub_editor() (
+  set -e # run in subshell
+  declare temp_dir version REPO="Thenujan-0/grub-editor"
+  if ! [ "$ARCH" = "x86_64" ] && ! [ "$ARCH" = "amd64" ]; then
+    abort 'Only amd64 and x86_64 are supported at this time.'
+  fi
+
+  version=$(get_latest_version_github "$REPO") # v1.0.0
+
+  asset=grub-editor_${version#v}-1_amd64.deb # grub-editor_1.0.0-1_amd64.deb - no other files are available.
+  # Download the .deb
+  temp_dir=$(mktemp -d) &&
+    (
+      # In a subshell, so runs at end of block
+      trap 'rm -rf $temp_dir' EXIT
+      set -e
+      local destination=$temp_dir/grub-editor.deb
+      log_github_install "$REPO" "$version" "$asset" "$destination"
+      curl -sSL "https://github.com/$REPO/releases/download/$version/$asset" -o "$destination"
+      sudo apt install "$destination"
+    )
+)
+
+if is_accessible_cmd apt && ! is_available_apt grub-editor; then
+  log_and_run "Installing grub-editor" install_grub_editor
+fi
