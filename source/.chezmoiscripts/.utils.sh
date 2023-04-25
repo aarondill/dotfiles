691491c519a1b5bb7ebb7db1d5ea1919c3c19091
aarondill <aaronsacks2006@gmail.com>
2023-04-25 12:00:13 -0500

    comments
    Former-commit-id: 127bf8914093558edf604d8cebc95f365557a7f9
    Former-commit-id: c07d468880a50b7756fbe487fb261207c14d84bb
    Former-commit-id: b80a29ac3826dde96abfbe5d2354397a8cafbade

--- source/.chezmoiscripts/.utils.sh
+++ source/.chezmoiscripts/.utils.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 # Source this file to get utilities
+(return 0 2>/dev/null) && sourced=1 || sourced=0
+if [ "$sourced" -eq 0 ]; then err "You should source this file. not run it."; fi
 
+## --------------------------------------------------------------------------------------------------
+## --------------------------------------------- Output ---------------------------------------------
+## --------------------------------------------------------------------------------------------------
+
+# log "hello world"
 function log() { printf '%s\n' "$@"; }
+# err "goodbye world"
 function err() { printf '%s\n' "$@" >&2; }
+# success - no arguments
 function success() { printf "$(tput setaf 2)%s$(tput sgr0)\n" "Success!"; }
+
+## --------------------------------------------------------------------------------------------------
+## ------------------------------------------ Control Flow ------------------------------------------
+## --------------------------------------------------------------------------------------------------
+
+# abort "something went wrong!"
 function abort() { err "$@" && exit 2; }
+# abort0 "something went wrong!" -- exits 0
 function abort0() { err "$@" && exit 0; }
+# confirm "do you really want to do that?"
 function confirm() {
   read -rep "$* (Y/n) " confirmation
   if [[ -z "$confirmation" || "${confirmation,,}" =~ ^\s*y(es)?\s*$ ]]; then
@@ -13,20 +30,8 @@ function confirm() {
   fi
   return 1
 }
-function lower() { local t && t="$(cat -)" && printf '%s' "${t,,}"; }
-function first_lower() { local t && t="$(cat -)" && printf '%s' "${t,}"; }
-function upper() { local t && t="$(cat -)" && printf '%s' "${t^^}"; }
-function first_upper() { local t && t="$(cat -)" && printf '%s' "${t^}"; }
-
-# returns 0 if all cmds are available, 1 otherwise
-function is_accessible_cmd() {
-  declare -i failed=0
-  for cmd; do command -v "$cmd" &>/dev/null || failed=1; done
-  return $failed
-}
-
+# Usage: log_and_run "Installing something" apt install -y something
 function log_and_run() {
-  # Usage: log_and_run "Installing something" apt install -y something
   local task command args
   task="$1"
   command="$2"
@@ -38,7 +43,32 @@ function log_and_run() {
     success
   ) || err "Something went wrong while ${task,}!" # Lowercase
 }
+# returns 0 if all cmds are available, 1 otherwise
+# is_accessible_cmd apt ls git
+function is_accessible_cmd() {
+  declare -i failed=0
+  for cmd; do command -v "$cmd" &>/dev/null || failed=1; done
+  return $failed
+}
 
+## --------------------------------------------------------------------------------------------------
+## ------------------------------------------- Text Utils -------------------------------------------
+## --------------------------------------------------------------------------------------------------
+
+# lower <<<"HELLO" -> "hello"
+function lower() { local t && t="$(cat -)" && printf '%s' "${t,,}"; }
+# first_lower <<<"HELLO" -> "hELLO"
+function first_lower() { local t && t="$(cat -)" && printf '%s' "${t,}"; }
+# upper <<<"hello" -> "HELLO"
+function upper() { local t && t="$(cat -)" && printf '%s' "${t^^}"; }
+# first_upper <<<"hello" -> "Hello"
+function first_upper() { local t && t="$(cat -)" && printf '%s' "${t^}"; }
+
+## --------------------------------------------------------------------------------------------------
+## --------------------------------------------- GitHub ---------------------------------------------
+## --------------------------------------------------------------------------------------------------
+
+# get_latest_version_github "someone/something" -> v1.2.3 (tagname)
 function get_latest_version_github() (
   set -e            # in subshell
   declare REPO="$1" # combined $OWNER/$REPO
@@ -49,7 +79,7 @@ function get_latest_version_github() (
   fi
   echo "$version"
 )
-# usage: `install_from_github aaron/example latest example.sh /usr/local/bin/example`
+# install_from_github aaron/example latest example.sh /usr/local/bin/example
 function install_from_github() (
   set -e # runs in subshell, so doesn't affect outside
   local github_repo=$1 version=$2 asset=$3 destination=$4 TMP
@@ -83,6 +113,10 @@ function log_github_install() {
   log "Installing $github_repo version $version ($asset) to $destination"
 }
 
+## --------------------------------------------------------------------------------------------------
+## -------------------------------------------- Download --------------------------------------------
+## --------------------------------------------------------------------------------------------------
+
 # download_file [sudo] <file> <destination> [mode]
 function download_file() {
   local cmd=() file_url=$1 dest=$2 mode=$3 need_sudo=''
@@ -109,8 +143,15 @@ function download_file() {
   "${cmd[@]}"      # Run the constructed command
 }
 
-KERNEL=$(uname -s) # eg: Linux
-ARCH=$(uname -m)   # eg: x86_64
+## --------------------------------------------------------------------------------------------------
+## ------------------------------------------- Variables --------------------------------------------
+## --------------------------------------------------------------------------------------------------
+
+# eg: Linux
+KERNEL=$(uname -s)
+# eg: x86_64
+ARCH=$(uname -m)
+# eg: Ubuntu
 OS=$(lsb_release -si)
 export ARCH KERNEL OS
 
