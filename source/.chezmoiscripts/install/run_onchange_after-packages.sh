64db46bc1bc922f3bc04f1b96e48093108eadcc2
aarondill <aaronsacks2006@gmail.com>
2023-04-25 12:16:59 -0500

    move bitwarden to own file
    Former-commit-id: d283a143d0c76123e54470a099c2e6ec7dc592ad
    Former-commit-id: 3fc970a7d9367eb0588cd7db00725415e60cd586
    Former-commit-id: 6ce6315222f143bccaee493e9215ec27c90ac527

--- source/.chezmoiscripts/install/run_onchange_after-packages.sh
+++ source/.chezmoiscripts/install/run_onchange_after-packages.sh
@@ -5,29 +5,6 @@ SOURCE_DIR=$(chezmoi source-path)
 . "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
 
 #region ## Actual Code {{{1
-#region ### Bitwarden {{{2
-function install_bitwarden_desktop() {
-  declare DESTINATION=/usr/local/bin/bitwarden
-  sudo curl -sSL 'https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=appimage' -o "$DESTINATION"
-  sudo chown root "$DESTINATION"
-  sudo chmod +x "$DESTINATION"
-}
-
-function install_bitwarden_cli() {
-  declare temp_dir
-  temp_dir=$(mktemp -d) && (
-    # Executed in a subshell, so this will run on end of block
-    trap 'rm -rf $temp_dir' EXIT
-    set -e
-
-    curl -sSL 'https://vault.bitwarden.com/download/?app=cli&platform=linux' -o "$temp_dir/bw.zip"
-    unzip -qq "$temp_dir/bw.zip" -d "$temp_dir"
-    sudo mv -f "$temp_dir/bw" /usr/local/bin/bw
-    sudo chmod +x /usr/local/bin/bw
-  )
-}
-
-#endregion
 #region ### FZF {{{2
 function install_fzf() {
   local REPO=junegunn/fzf BINLOCATION=${BINLOCATION:-/usr/bin}
@@ -218,11 +195,6 @@ if ! confirm "Would you like to install some things?"; then
   err 'Aborting.'
   exit 0
 fi
-# Ignore if already installed, updates itself
-is_accessible_cmd bitwarden ||
-  log_and_run "Installing bitwarden desktop" install_bitwarden_desktop
-
-log_and_run "Installing bitwarden CLI" install_bitwarden_cli
 
 log_and_run 'Installing fzf' install_fzf
 
---
+++ source/.chezmoiscripts/install/run_onchange_after_bitwarden.sh
@@ -0,0 +1,26 @@
+function install_bitwarden_desktop() {
+  declare DESTINATION=/usr/local/bin/bitwarden
+  sudo curl -sSL 'https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=appimage' -o "$DESTINATION"
+  sudo chown root "$DESTINATION"
+  sudo chmod +x "$DESTINATION"
+}
+
+function install_bitwarden_cli() {
+  declare temp_dir
+  temp_dir=$(mktemp -d) && (
+    # Executed in a subshell, so this will run on end of block
+    trap 'rm -rf $temp_dir' EXIT
+    set -e
+
+    curl -sSL 'https://vault.bitwarden.com/download/?app=cli&platform=linux' -o "$temp_dir/bw.zip"
+    unzip -qq "$temp_dir/bw.zip" -d "$temp_dir"
+    sudo mv -f "$temp_dir/bw" /usr/local/bin/bw
+    sudo chmod +x /usr/local/bin/bw
+  )
+}
+
+# Ignore if already installed, updates itself
+is_accessible_cmd bitwarden ||
+  log_and_run "Installing bitwarden desktop" install_bitwarden_desktop
+
+log_and_run "Installing bitwarden CLI" install_bitwarden_cli
