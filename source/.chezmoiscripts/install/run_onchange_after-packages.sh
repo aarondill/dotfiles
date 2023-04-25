0fab97a2f28dde77297ac1c3d66b944a01fe6106
aarondill <aaronsacks2006@gmail.com>
2023-04-25 12:23:38 -0500

    move lazygit to own file
    Former-commit-id: 7cc582f4ac8506bc5d4a86955bdc0d1b2d638f3f
    Former-commit-id: 239d6ce675ebc1a2018e59bcbd04802e3e2a5d09
    Former-commit-id: d714e9f7806cb85d0fbf455e2cbcfac8dda55148

--- source/.chezmoiscripts/install/run_onchange_after-packages.sh
+++ source/.chezmoiscripts/install/run_onchange_after-packages.sh
@@ -35,29 +35,6 @@ function install_fx() {
 
 #endregion
 #endregion
-#region ### Lazygit {{{2
-function install_lazygit() (
-  set -e
-  local LAZYGIT_VERSION REPO FILE DESTINATION
-  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-  FILE="lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-  BINDIR=/usr/local/bin
-  REPO='jesseduffield/lazygit'
-
-  log_github_install "$REPO" "$LAZYGIT_VERSION" "$FILE" "$BINDIR"
-
-  TMP=$(mktemp)
-  trap 'rm -f "$TMP"' EXIT
-
-  curl -SsLf "https://github.com/$REPO/releases/download/v$LAZYGIT_VERSION/$FILE" -o "$TMP"
-  # output to destination
-  tar -xvz -C "$BINDIR" -f "$TMP" lazygit
-  sudo chmod +x "$BINDIR/lazygit"
-
-  rm -f "$TMP" && trap '' EXIT # Cleanup
-)
-#endregion
-
 #region ### Java {{{2
 function install_java() (
   set -e
@@ -81,8 +58,6 @@ function install_java() (
 ### Non-Funcion Code {{{1
 
 log_and_run 'installing fx' install_fx
-
-log_and_run 'installing lazygit' install_lazygit
 
 log_and_run 'installing java' install_java
 
---
+++ source/.chezmoiscripts/install/run_onchange_after_lazygit.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+# Source utils
+SOURCE_DIR=$(chezmoi source-path)
+# shellcheck source=../.utils.sh
+. "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
+
+function install_lazygit() (
+  set -e
+  local LAZYGIT_VERSION REPO FILE
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+  FILE="lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  BINDIR=/usr/local/bin
+  REPO='jesseduffield/lazygit'
+
+  log_github_install "$REPO" "$LAZYGIT_VERSION" "$FILE" "$BINDIR"
+
+  TMP=$(mktemp)
+  trap 'rm -f "$TMP"' EXIT
+
+  curl -SsLf "https://github.com/$REPO/releases/download/v$LAZYGIT_VERSION/$FILE" -o "$TMP"
+  # output to destination
+  tar -xvz -C "$BINDIR" -f "$TMP" lazygit
+  sudo chmod +x "$BINDIR/lazygit"
+
+  rm -f "$TMP" && trap '' EXIT # Cleanup
+)
+
+log_and_run 'installing lazygit' install_lazygit
