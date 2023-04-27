2f998bb4cd57e9d7c7125f73eeed6d94d491fa2e
aarondill <aaronsacks2006@gmail.com>
2023-04-27 08:25:00 -0500

    update-root
    Former-commit-id: 5212f59d1d58fe60a306c1f237172a5c1d4e55de
    Former-commit-id: ba32e4aced8229ccd40ccc00c4f1eea8097d8b1e
    Former-commit-id: e6da30512d554c15c01d2382a25c86c029c0291f

---
+++ source/.chezmoiscripts/run_once_after_05-update-root.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# this tars the '~/.root' directory and unzips it at '/'
+# This script *MUST* ask confirmation! These are breaking changes!
+# This script should aslo suggest running 'sudo update-grub' and 'sudo update-initramfs -u'
+set -e
+# Source utils
+SOURCE_DIR=$(chezmoi source-path)
+# shellcheck source=.utils.sh
+. "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
+
+ROOTBACKUP="$HOME/.root"
+TARDESTFILE="$(mktemp)"
+trap 'rm -f "$TARDESTFILE"' EXIT
+
+confirm 'Would you like to update / with the contents of %s?' "$ROOTBACKUP" || abort0 "Aborting"
+
+# Find safe backup directory
+BACKUPDIR_ROOT="/backup"
+BACKUPDIR="$BACKUPDIR_ROOT"
+declare -i I=1
+while [ -d "$BACKUPDIR" ]; do
+  BACKUPDIR="$BACKUPDIR_ROOT-$I"
+  I=$((I + 1))
+done
+
+declare -i BACKUP=0
+confirm 'Would you like to keep backups of overwritten files in / in %s?' "$BACKUPDIR" && BACKUP=1
+
+# Don't record /home/$USER/.root/, instead start at subdirs
+find "$ROOTBACKUP"/ -maxdepth 1 -mindepth 1 -type d -printf '%f\0' |
+  tar -cf "$TARDESTFILE" -C "$ROOTBACKUP"/ --null -T -
+
+if [ $BACKUP -ne 0 ]; then
+  # List files
+  FILES=$(tar -tf "$TARDESTFILE" | sed 's/^/\//')
+  for file in $FILES; do
+    # Ensure unreadable files are still copied
+    if sudo test -f "$file"; then
+      # Ensure destination directory exists -- sudo so can be located at root
+      sudo mkdir -p "$BACKUPDIR/$(dirname -- "$file")"
+      # copy the file to the destination
+      sudo cp -T -- "$file" "$BACKUPDIR/$file"
+    fi
+  done
+fi
+
+log "Last chance to safely stop overwrite of root. Press ctrl-c to cancel."
+
+# On ctrl-c, previous trap will occur, removing temporary file. backup will remain, but not a problem.
+secs=$((5))
+while [ $secs -gt 0 ]; do
+  echo -ne "$secs\033[0K\r"
+  sleep 1
+  secs=$((secs - 1))
+done
+
+# Important! Overwrites files in ROOT!
+sudo tar --no-same-owner -xf "$TARDESTFILE" -C /
