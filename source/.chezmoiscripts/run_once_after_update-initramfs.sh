98986d5a96fd64491f61c13860d0711c04d766f5
aarondill <aaronsacks2006@gmail.com>
2023-04-26 19:54:00 -0500

    add initramfs/.../resume during update
    Former-commit-id: 139752b840c7e45077135fb27fb8a93a2bf4e68d
    Former-commit-id: 0c0239d23586628e4c9111da43ba74493e81ef83
    Former-commit-id: 73ce705934a0c265e370909c9e9f82337b51bbfb

---
+++ source/.chezmoiscripts/run_once_after_update-initramfs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# this finds the largest swap partition and *offers* to replace important instances of the backed up source with the uuid
+set -e
+# Source utils
+SOURCE_DIR=$(chezmoi source-path)
+# shellcheck source=.utils.sh
+. "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
+
+RESUME_FILE=/etc/initramfs-tools/conf.d/resume
+
+BIG_SWAP_PATH=$(tail -n+2 </proc/swaps | LC_ALL=C sort -t$'\t' -nk3 | tail -n1 | awk '{print $1}')
+
+[ -f "$RESUME_FILE" ] && WARNING="(This will overwrite $RESUME_FILE)" || WARNING=''
+
+confirm "Would you like to use $BIG_SWAP_PATH as your swap to resume from? $WARNING" || abort0 'Aborting'
+if [ -f "$RESUME_FILE" ]; then
+  log "overwriting $RESUME_FILE"
+  rm -f -- "$RESUME_FILE"
+fi
+
+BIG_SWAP_UUID="$(blkid -o value -s UUID "$BIG_SWAP_PATH")"
+printf 'RESUME=UUID=%s\n' "$BIG_SWAP_UUID" >"$RESUME_FILE"
+# update the initramfs-tools
+sudo update-initramfs -u
+
+success
