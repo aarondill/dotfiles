38d26aaeedabcc14a13dba5ed59d81655e68d2e8
aarondill <aaronsacks2006@gmail.com>
2023-04-30 13:40:09 -0500

    Update source/.bashrc.d/60_PS1/00_git Update source/.bashrc.d/60_PS1/01_starship.sh
    Former-commit-id: 8bb8d5791ba4dc17862e06f7142e2831afa5bc8e
    Former-commit-id: 50cbc99b4f57adb32a277ec6f45e1501e8eec4d9
    Former-commit-id: 315b44945c66866d1eaa4a52d1ec3d08c9111f47

--- source/exact_dot_bashrc.d/exact_60_PS1/01_starship.sh
+++ source/exact_dot_bashrc.d/exact_60_PS1/01_starship.sh
@@ -17,20 +17,32 @@ starship_precmd || true
 
 # Set the title of the window
 function set_win_title() {
+  local cwd title end who git_available git_base git_base_path
+  git_available=
+  if { command -v git && git --version; } &>/dev/null; then
+    git_available=1
+  fi
+
   # Create (GIT: TOP-LEVEL) for titlebar
-  local git_base=""
-  if [[ $git_available ]] && git rev-parse &>/dev/null; then # In git
-    git_base=" (GIT: "
+  git_base=""
+  if [ "$git_available" = 1 ] && git rev-parse &>/dev/null; then # In git
+    git_base=" (git: "
     if [[ "$(git rev-parse --is-inside-work-tree)" = "true" ]]; then # In work tree
-      git_base+=$(basename -- "$(git rev-parse --show-toplevel 2>/dev/null)")
+      git_base_path="$(git rev-parse --show-toplevel 2>/dev/null)"
+      git_base+=$(basename -- "$git_base_path")
     else # In .git
-      git_base+=$(basename -- "$(realpath -- "$(git rev-parse --git-dir)/..")")
+      git_base_path="$(git rev-parse --git-dir)/.."
+      git_base+=$(basename -- "$(realpath -- "$git_base_path")")
     fi
     git_base+=")"
   fi
   # Escapes for setting titlebar
-  local title='\[\e]0;' end='\a\]'
-  local title_bar="$title\W$git_base: \u$end\${debian_chroot:+(\$debian_chroot)}"
+  title='\033]0;' end='\007'
+  cwd=$PWD
+  if [ "$cwd" = "$HOME" ]; then cwd='~'; else cwd=$(basename -- "$cwd"); fi
+  who=$(whoami)
+
+  local title_bar="$title$who: $cwd$git_base$end"
   printf "%b" "$title_bar"
 }
 # Please starship, do it!
