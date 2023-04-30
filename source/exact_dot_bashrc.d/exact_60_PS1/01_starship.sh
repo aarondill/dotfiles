b6a09c8876873c17a2e02a75971349892e48c865
aarondill <aaronsacks2006@gmail.com>
2023-04-30 13:19:33 -0500

    Update source/.bashrc.d/60_PS1/01_starship.sh Update source/.config/starship.toml
    Former-commit-id: 7b10c53acb3e0d1869b2df0b5a902d88c355448f
    Former-commit-id: 3918f5dc1eecc382623f5e6f38872a4dcb24a9f8
    Former-commit-id: b1f0270a86c7532c1e95c419c299611fc9e949a5

--- source/exact_dot_bashrc.d/exact_60_PS1/01_starship.sh
+++ source/exact_dot_bashrc.d/exact_60_PS1/01_starship.sh
@@ -1,37 +1,7 @@
 #!/usr/bin/env bash
-# Only if custom is not in use
 if ! [ "$DISABLE_CUSTOM_PS1" = 1 ]; then
   return
 fi
-
-# For compat. Starship will set this
 unset PS1
-
-# Init starship
 eval "$(starship init bash)"
-
-# Force first load to ensure that PS1 gets set
-true
-# or true, I don't care if this fails, starship should handle itself.
 starship_precmd || true
-
-# Set the title of the window
-function set_win_title() {
-  # Create (GIT: TOP-LEVEL) for titlebar
-  local git_base=""
-  if [[ $git_available ]] && git rev-parse &>/dev/null; then # In git
-    git_base=" (GIT: "
-    if [[ "$(git rev-parse --is-inside-work-tree)" = "true" ]]; then # In work tree
-      git_base+=$(basename -- "$(git rev-parse --show-toplevel 2>/dev/null)")
-    else # In .git
-      git_base+=$(basename -- "$(realpath -- "$(git rev-parse --git-dir)/..")")
-    fi
-    git_base+=")"
-  fi
-  # Escapes for setting titlebar
-  local title='\[\e]0;' end='\a\]'
-  local title_bar="$title\W$git_base: \u$end\${debian_chroot:+(\$debian_chroot)}"
-  echo -ne "\033]0; $(basename "$PWD") \007"
-}
-# Please starship, do it!
-export starship_precmd_user_func="set_win_title"
