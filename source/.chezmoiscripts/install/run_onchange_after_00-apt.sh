ef90a3361c3b375e5d764c8a230ae38ed86353ac
aarondill <aaronsacks2006@gmail.com>
2023-05-01 14:59:14 -0500

    improve apt script
    Former-commit-id: bd5224253b67e6617cdda68737dc40dea64e3953
    Former-commit-id: 32eefb683d351c67bd50a43f32f3a0d11d13c38b
    Former-commit-id: dd7a4ca872156d879f635dbbc9c3c28b1cfd9e18

--- source/.chezmoiscripts/install/run_onchange_after_00-apt.sh
+++ source/.chezmoiscripts/install/run_onchange_after_00-apt.sh
@@ -1,32 +1,34 @@
 #! /usr/bin/env bash
+set -e
 # Source utils
 SOURCE_DIR=$(chezmoi source-path)
 # shellcheck source=../.utils.sh
 . "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
 
+apt=$(which nala 2>/dev/null || which apt 2>/dev/null)
+
 function remove_if_installed_apt() {
-  local package
+  local package packages=()
   for package; do
     if dpkg -s "$package" &>/dev/null; then
-      sudo apt remove -y -- "$package"
+      packages+=("$package")
     fi
   done
+  if [ ${#packages[@]} -eq 0 ]; then return 0; fi
+  sudo "$apt" remove -y -- "${packages[@]}"
 }
 function install_if_available_apt() {
-  declare -a available_packages
-  available_packages=()
-  local package
+  local packages=() package
   for package; do
     if is_available_apt "$package"; then
-      available_packages+=("$package")
+      packages+=("$package")
       log "Installing $package"
     else
       err "Could not find '$package' in apt repos"
     fi
   done
-  apt=$(which nala 2>/dev/null || which apt 2>/dev/null)
-  sudo "$apt" update
-  sudo "$apt" install -y -- "${available_packages[@]}"
+  if [ ${#packages[@]} -eq 0 ]; then return 0; fi
+  sudo "$apt" install -y -- "${packages[@]}"
 }
 
 function install_nala() {
@@ -39,9 +41,7 @@ function install_apt_packages() {
     dconf-editor duf fwts gh git golang-go grep ifupdown inotify-tools less make \
     neofetch neovim net-tools openvpn p7zip-full python3-neovim rsync shfmt tlp \
     trash-cli tree util-linux xclip xdg-utils zip zoxide htop apt-file speedtest-cli \
-    ripgrep
-  is_accessible_cmd apt &&
-    install_if_available_apt apt-clone aptitude
+    ripgrep apt-clone aptitude
 }
 
 function install_graphical_apt_packages() {
@@ -64,9 +64,8 @@ installed_or_log apt && {
   log_and_run 'Installing apt packages' install_apt_packages
   log_and_run 'Installing graphical (gnome) apt packages' install_graphical_apt_packages
   # Gnome comes with it, but I don't want it.
-  remove_if_installed_apt gnome-characters
   # Replace the existing cheese package with the flatpak package
-  remove_if_installed_apt cheese
+  remove_if_installed_apt gnome-characters cheese
   # Install vim symlink to nvim
   if command -v nvim &>/dev/null; then
     sudo update-alternatives --install /usr/bin/vim neovim "$(which nvim)" 100 || true
