df92c8e71e8a8faed11680c36c3e17058b1143d0
aarondill <aaronsacks2006@gmail.com>
2023-05-01 11:06:28 -0500

    vim pls
    Former-commit-id: 273b28695981780aa174b554b07a34707649c37f
    Former-commit-id: 3b3fb9b370efc6ca16618a9e60930684f923ec62
    Former-commit-id: 22bc40924bdc05ab9e13d755fedcd4faf05a5d94

--- source/.chezmoiscripts/install/run_onchange_after_00-apt.sh
+++ source/.chezmoiscripts/install/run_onchange_after_00-apt.sh
@@ -62,4 +62,8 @@ installed_or_log apt && {
   remove_if_installed_apt gnome-characters
   # Replace the existing cheese package with the flatpak package
   remove_if_installed_apt cheese
+  # Install vim symlink to nvim
+  if command -v nvim &>/dev/null; then
+    sudo update-alternatives --install vim neovim "$(which nvim)" 100 || true
+  fi
 }
