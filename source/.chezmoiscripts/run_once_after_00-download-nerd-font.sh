70d47e8732c76d31c310732c2209bf4c8aad71da
aarondill <aaronsacks2006@gmail.com>
2023-05-01 22:48:47 -0500

    install nerdfonts
    this only runs if UbuntuMono hasn't already been installed
    
    
    Former-commit-id: 6f5390f21283876fbd8a8bac5a26c71895ff9c1a
    Former-commit-id: c154321ce1ec40924ddf50a152dd8b81dc418229
    Former-commit-id: 964a14db63d2fb23c8bb0678ff8cbd4b1c3e4c1d

---
+++ source/.chezmoiscripts/run_once_after_00-download-nerd-font.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Source utils
+SOURCE_DIR=$(chezmoi source-path)
+# shellcheck source=.utils.sh
+. "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
+
+log "Installing NerdFonts"
+
+NERDFONTSDIR=~/.local/share/fonts/NerdFonts/
+REGULAR_FILE=UbuntuMonoNerdFontMono-Regular.ttf
+# Test arbitrary file to ensure that nerd fonts is installed
+if [ -f "$NERDFONTSDIR/$REGULAR_FILE" ]; then abort0 "NerdFonts are already installed!"; fi
+
+log "Downloading nerd fonts repository"
+TMP_DIR=$(mktemp -d)
+trap 'rm -rf "$TMP_DIR"' EXIT
+
+git clone 'https://github.com/ryanoasis/nerd-fonts' "$TMP_DIR"
+"$TMP_DIR/install.sh" --use-single-width-glyphs --install-to-user-path UbuntuMono
+
+rm -rf "$TMP_DIR" && trap '' EXIT
+success
