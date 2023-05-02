1347dcddfd8d80bfc395a8da6edc387ce02f4172
aarondill <aaronsacks2006@gmail.com>
2023-05-01 23:21:25 -0500

    better logging
    Former-commit-id: ec35accdd5e8510a256e0221375499b30bd45286
    Former-commit-id: 10fa88e17b5574bedcb6badb31c8e09d9a555da3
    Former-commit-id: 95b77fef3da6178fd8a14c4618144255d19eafce

--- source/.chezmoiscripts/run_once_after_00-download-nerd-font.sh
+++ source/.chezmoiscripts/run_once_after_00-download-nerd-font.sh
@@ -7,10 +7,11 @@ SOURCE_DIR=$(chezmoi source-path)
 
 log "Installing NerdFonts"
 
-NERDFONTSDIR=~/.local/share/fonts/NerdFonts/
+NERD_FONT_TO_INSTALL=UbuntuMono
+NERD_FONTS_DIR=~/.local/share/fonts/NerdFonts/
 REGULAR_FILE=UbuntuMonoNerdFontMono-Regular.ttf
 # Test arbitrary file to ensure that nerd fonts is installed
-if [ -f "$NERDFONTSDIR/$REGULAR_FILE" ]; then abort0 "NerdFonts are already installed!"; fi
+if [ -f "$NERD_FONTS_DIR/$REGULAR_FILE" ]; then abort0 "NerdFonts are already installed!"; fi
 
 log "Downloading nerd fonts repository"
 TMP_DIR=$(mktemp -d)
@@ -18,11 +19,13 @@ trap 'rm -rf "$TMP_DIR"' EXIT
 
 # Only download toplevel files
 git clone --sparse --filter=blob:none 'https://github.com/ryanoasis/nerd-fonts' "$TMP_DIR"
-# Download specific files for UbuntuMono fonts
-git -C "$TMP_DIR" sparse-checkout add patched-fonts/UbuntuMono
+log "Downloading $NERD_FONT_TO_INSTALL fonts"
+# Download specific files for $NERD_FONT_TO_INSTALL fonts
+git -C "$TMP_DIR" sparse-checkout add "patched-fonts/$NERD_FONT_TO_INSTALL"
 
+log "Installing $NERD_FONT_TO_INSTALL fonts"
 # ALl files should be downloaded already!
-"$TMP_DIR/install.sh" --use-single-width-glyphs --install-to-user-path UbuntuMono
+"$TMP_DIR/install.sh" --use-single-width-glyphs --install-to-user-path "$NERD_FONT_TO_INSTALL"
 
 rm -rf "$TMP_DIR" && trap '' EXIT
 success
