7aca29df53114e352bd1b5911d04edd84c0b7e19
aarondill <aaronsacks2006@gmail.com>
2023-05-03 23:29:04 -0500

    Update source/.config/wezterm/plugins/toggle-fullscreen.lua
    Former-commit-id: d2035e24bb3f0dc1ac5b7ba32045954f986359c9
    Former-commit-id: 263e7b350a38b213c426a2f799cedab7fa79c64a
    Former-commit-id: 9d85c40b3f74be78108fca7fba95fa012fac5f29

--- source/private_dot_config/exact_wezterm/exact_plugins/toggle-fullscreen.lua
+++ source/private_dot_config/exact_wezterm/exact_plugins/toggle-fullscreen.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 
 return require("util").define_key({
-	key = "n",
-	mods = "SHIFT|CTRL",
+	key = "K",
+	mods = "CTRL",
 	action = wezterm.action.ToggleFullScreen,
 })
