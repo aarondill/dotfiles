5997d3d9ede10d329f32d91bcff6bf8bae610720
aarondill <aaronsacks2006@gmail.com>
2023-05-08 17:12:43 -0500

    install tealdeer
    Former-commit-id: 0194fc529476e2a592fead2221d2bd072fc40600
    Former-commit-id: d4eb82712d4f8e1bd1d40a080b975fa4936b443f
    Former-commit-id: 50e76447d64a5157c88e93ea0f08ac7bedc376ce

---
+++ source/.chezmoiscripts/install/run_onchange_after_35-tealdeer.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+set -eu
+# Source utils
+SOURCE_DIR=$(chezmoi source-path)
+# shellcheck source=../.utils.sh
+. "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
+
+FILE="tealdeer-linux-x86_64-musl"
+DESTINATION=/usr/local/bin/tldr
+REPO='dbrgn/tealdeer'
+
+install_from_github $REPO latest "$FILE" "$DESTINATION"
