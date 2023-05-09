854a86e58500c004d22c47d5989d5b57e58945fd
aarondill <aaronsacks2006@gmail.com>
2023-05-09 09:30:41 -0500

    Add source/.local/bin/script_utils.sh
    Former-commit-id: 297c627c9222cc97cf43c1b4bbfcd5c95c3255ac
    Former-commit-id: aa8b83bcf0abb9778ea973d467ce99f80ed5ef5c
    Former-commit-id: c4fec24419aabf21e28a2d0179f321efbaf8ba70

---
+++ source/dot_local/exact_bin/executable_script_utils.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# ~/.local/bin/script_utils.sh
+#
+# A template script for writing good scripts
+# could be improved, but 🤷
+#
+set -eu
+
+THIS=script_utils.sh
+usage() {
+  cat <<EOF
+$THIS [options] [--] [arguments]
+                  
+This does <SOMETHING>
+
+Options:
+-h, --help        show this message
+-s, --long=long   do something with \$long
+EOF
+  return 0
+}
+
+log() { printf '%s\n' "$@"; }
+err() { printf "$THIS: %s\n" "$@" >&2; }
+abort() { err "$1" && exit "${2:-1}"; }
+
+# Sets the variable $args to an array of positional arguments
+# Usage: parse_args "$@"
+function parse_args() {
+  # If awaiting an argument
+  next='' next_arg='' end=false args=()
+  for arg in "$@"; do
+    if [ $end = true ]; then
+      args+=("$arg") && continue
+    elif [ -n "$next" ]; then
+      declare "$next"="$arg" next=''
+    else
+      case "$arg" in
+      --help | -h) usage && exit 0 ;;
+      --) end=true ;;
+      -s | --long) next=long_setting next_arg="$arg" ;;
+      --long=*) long_setting="${arg#--save=}" ;;
+      --*) abort "Invalid option -- ${arg#--}" 2 ;;
+      -*) abort "Invalid option -- ${arg#-}" 2 ;;
+      *) args+=("$arg") ;;
+      esac
+    fi
+  done
+  # If awaiting an argument, but end of args
+  if [ -n "$next" ]; then abort "$next_arg requires an argument" 2; fi
+  unset end next next_arg
+}
+
+long_setting=''
+parse_args "$@"
+
+# Do something with empty positionals
+if [ "${#args[@]}" -eq 0 ]; then :; fi
+# Do something with --long
+echo "$long_setting"
