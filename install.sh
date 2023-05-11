a23255627be9eee1b786ec8e00138cc9267f2d07
aarondill <aaronsacks2006@gmail.com>
2023-05-11 17:13:34 -0500

    change default bindir
    Former-commit-id: 5f31026c0128d5cec425a9236218a0f5a7af5398
    Former-commit-id: 26646cb224bdd29275ee50b131a9e19c856c3d58
    Former-commit-id: 2f514faf2e43e34cc506d66cf7b6b358fee125d0

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ error() {
 }
 
 if ! chezmoi="$(command -v chezmoi)"; then
-  bin_dir="${HOME}/.local/bin"
+  bin_dir="${BINDIR:-/usr/local/bin}"
   chezmoi="${bin_dir}/chezmoi"
   log_task "Installing chezmoi to '${chezmoi}'"
   if command -v curl >/dev/null; then
