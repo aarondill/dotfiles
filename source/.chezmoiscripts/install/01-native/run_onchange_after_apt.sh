61c72ef3e301c36a6954d7574eda0a7ae7a44298
aarondill <aaronsacks2006@gmail.com>
2023-05-12 13:28:18 -0500

    refactor remove _apt,apt_ extentions
    Former-commit-id: 7bd83355d42a6ef24e442724f602421403493fb4
    Former-commit-id: a13ddf0f53a5f2bdc4e80a1abb42df9002de0c86
    Former-commit-id: 2434597fd43441f08434a06ee83826ed5ccc5ca2

--- source/.chezmoiscripts/install/01-native/run_onchange_after_apt.sh
+++ source/.chezmoiscripts/install/01-native/run_onchange_after_apt.sh
@@ -9,18 +9,23 @@ if [ -z "$APT" ]; then
   # STOP! if dependencies aren't installed, other things will fail, but can't use apt.
   abort0 "Apt is not installed, skipping apt install script"
 fi
-GNOME='gnome-shell'
 
-PACKAGES=(age anacron autopoint bat cmake command-not-found curl
+GNOME=$(which gnome-shell 2>/dev/null || printf '')
+
+PACKAGES=(
+  age anacron autopoint bat cmake command-not-found curl
   dconf-editor duf fwts gh git golang-go grep ifupdown inotify-tools less make
   neofetch neovim net-tools openvpn p7zip-full python3-neovim rsync shfmt tlp
-  trash-cli tree util-linux xclip xdg-utils zip zoxide htop apt-file speedtest-cli
-  ripgrep apt-clone aptitude libfuse2 hexedit luajit python3-pip ppa-purge xdotool)
-GRAPHICAL_PACKAGES=(dconf-editor flatpak gimp
-  gnome-shell-extension-manager gparted gucharmap luckybackup okular qtqr
-  zeal gnome-software gnome-software-plugin-flatpak gnome-tweaks gnome-boxes)
+  trash-cli tree util-linux xclip xdg-utils zip zoxide htop speedtest-cli
+  ripgrep libfuse2 hexedit luajit python3-pip ppa-purge xdotool
+)
+GRAPHICAL_PACKAGES=(
+  dconf-editor flatpak gimp gparted gucharmap luckybackup okular qtqr
+  zeal gnome-software gnome-software-plugin-flatpak gnome-boxes
+)
+GNOME_PACKAGES=(gnome-shell-extension-manager gnome-tweaks)
 
-function remove_if_installed_apt() {
+function remove_if_installed() {
   local package packages=()
   for package; do
     if dpkg -s "$package" &>/dev/null; then
@@ -30,39 +35,39 @@ function remove_if_installed_apt() {
   if [ ${#packages[@]} -eq 0 ]; then return 0; fi
   sudo "$APT" remove -y -- "${packages[@]}"
 }
-function install_if_available_apt() {
+function install_if_available() {
   local packages=() package
   for package; do
-    if is_available_apt "$package"; then
+    if is_available "$package"; then
       packages+=("$package")
     else
-      err "Could not find '$package' in apt repos"
+      err "Could not find '$package' in repos"
     fi
   done
   if [ ${#packages[@]} -eq 0 ]; then return 0; fi
   sudo "$APT" install -y -- "${packages[@]}"
 }
 
-function install_apt_packages() { install_if_available_apt "${PACKAGES[@]}"; }
+function install_packages() { install_if_available "${PACKAGES[@]}"; }
 
-function install_graphical_apt_packages() {
+function install_graphical_packages() {
+  local graphical_packages=("${GRAPHICAL_PACKAGES[@]}")
   # If gnome is not installed, ask confirmation, else just install
-  if ! is_installed_apt "$GNOME"; then
-    confirm "Gnome is not installed, would you still like to install gui applications?" || return 2
+  if [ -z "$GNOME" ]; then
+    confirm "Gnome is not installed, would you still like to install gui applications?"
+  else
+    graphical_packages+=("${GNOME_PACKAGES[@]}")
   fi
-  install_if_available_apt "${GRAPHICAL_PACKAGES[@]}"
+  install_if_available "${graphical_packages[@]}"
 }
 
-# Only run if apt is available - exits if false
-installed_or_log apt
-
 log_and_run 'installing neovim nightly ppa' sudo add-apt-repository -n -y -P ppa:neovim-ppa/unstable
-log_and_run 'Updating apt sources' sudo "$APT" update
-log_and_run 'Installing apt packages' install_apt_packages
-log_and_run 'Installing graphical (gnome) apt packages' install_graphical_apt_packages
+log_and_run 'Updating sources' sudo "$APT" update
+log_and_run 'Installing packages' install_packages
+log_and_run 'Installing graphical packages' install_graphical_packages
 # Gnome comes with it, but I don't want it.
 # Replace the existing cheese package with the flatpak package
-remove_if_installed_apt gnome-characters cheese
+remove_if_installed gnome-characters cheese
 # Install vim symlink to nvim
 if command -v nvim &>/dev/null; then
   if ! [ "$(basename -- "$(update-alternatives --query vim | grep 'Value: /.\+' | cut -d' ' -f2-)")" = nvim ]; then
