9883490389dd75fade6d744c2baff8b03b525aeb
aarondill <aaronsacks2006@gmail.com>
2023-05-12 13:11:48 -0500

    add UNTESTED pacman script
    This is untested, and many of these packages are likely under different names, which needs to be changed!
    
    
    Former-commit-id: 58e7270be12d26e389967b60bc52379ce8a9f8c4
    Former-commit-id: 4bf0c9225166ad115501d66c010e1c826bf6ace9
    Former-commit-id: f83e74606785006f93d80d8d04333aeafc3dffeb

---
+++ source/.chezmoiscripts/install/01-native/run_onchange_after_pacman.sh
@@ -0,0 +1,73 @@
+#! /usr/bin/env bash
+set -euC -o pipefail
+# Source utils
+SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
+# shellcheck source=../../.utils.sh
+. "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
+
+PACMAN=$(which pacman 2>/dev/null || printf '')
+
+if [ -z "$PACMAN" ]; then
+  # STOP! deps may not be installed, hopefully handled elsewhere
+  abort0 "pacman is not installed, skipping pacman installation"
+fi
+
+GNOME=$(which gnome-shell 2>/dev/null || printf '')
+
+PACKAGES=(
+  age anacron autopoint bat cmake command-not-found curl
+  dconf-editor duf fwts gh git golang-go grep ifupdown inotify-tools less make
+  neofetch neovim net-tools openvpn p7zip-full python3-neovim rsync shfmt tlp
+  trash-cli tree util-linux xclip xdg-utils zip zoxide htop speedtest-cli
+  ripgrep libfuse2 hexedit luajit python3-pip ppa-purge xdotool
+)
+GRAPHICAL_PACKAGES=(
+  dconf-editor flatpak gimp gparted gucharmap luckybackup okular qtqr
+  zeal gnome-software gnome-software-plugin-flatpak gnome-boxes
+)
+GNOME_PACKAGES=(gnome-shell-extension-manager gnome-tweaks)
+
+function remove_if_installed() {
+  local package packages=()
+  for package; do
+    if "$PACMAN" -Qi "$package" >/dev/null; then
+      packages+=("$package")
+    fi
+  done
+  if [ ${#packages[@]} -eq 0 ]; then return 0; fi
+  sudo "$PACMAN" remove -y -- "${packages[@]}"
+}
+function install_if_available() {
+  local packages=() package
+  for package; do
+    if is_available "$package"; then
+      packages+=("$package")
+    else
+      err "Could not find '$package'"
+    fi
+  done
+  if [ ${#packages[@]} -eq 0 ]; then return 0; fi
+  sudo "$PACMAN" install -y -- "${packages[@]}"
+}
+
+function install_packages() { install_if_available "${PACKAGES[@]}"; }
+
+function install_graphical_packages() {
+  local graphical_packages=("${GRAPHICAL_PACKAGES[@]}")
+  # If gnome is not installed, ask confirmation, else just install
+  if [ -z "$GNOME" ]; then
+    confirm "Gnome is not installed, would you still like to install gui applications?"
+  else
+    graphical_packages+=("${GNOME_PACKAGES[@]}")
+  fi
+  install_if_available "${graphical_packages[@]}"
+}
+
+# no install neovim latest, bc should already be
+log_and_run 'Installing packages' install_packages
+log_and_run 'Installing graphical packages' install_graphical_packages
+# Gnome comes with it, but I don't want it.
+# Replace the existing cheese package with the flatpak package
+remove_if_installed gnome-characters cheese
+# Install vim symlink to nvim - throws is /usr/bin/vim is defined.
+if which nvim &>/dev/null; then ln -s -T "$(which nvim)" /usr/bin/vim; fi
