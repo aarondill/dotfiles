7bfa5525667254c64a16b2578306bf7261948e71
aarondill <aaronsacks2006@gmail.com>
2023-05-12 12:43:58 -0500

    refactor apt installer
    Former-commit-id: e7635c48091ae213c3cfe273d2bed48e79c9a749
    Former-commit-id: 0210525a5b31d5ca6d425d5f07e56ff8ac9afaf0
    Former-commit-id: f295de55dbdbf6c0a4d0ea24b70f05a35bf7aa9b

--- source/.chezmoiscripts/install/run_onchange_after_01-apt.sh
+++ source/.chezmoiscripts/install/run_onchange_after_01-apt.sh
@@ -5,6 +5,15 @@ SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
 # shellcheck source=../.utils.sh
 . "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
 
+PACKAGES=(age anacron autopoint bat cmake command-not-found curl
+  dconf-editor duf fwts gh git golang-go grep ifupdown inotify-tools less make
+  neofetch neovim net-tools openvpn p7zip-full python3-neovim rsync shfmt tlp
+  trash-cli tree util-linux xclip xdg-utils zip zoxide htop apt-file speedtest-cli
+  ripgrep apt-clone aptitude libfuse2 hexedit luajit python3-pip ppa-purge xdotool)
+GRAPHICAL_PACKAGES=(dconf-editor flatpak gimp
+  gnome-shell-extension-manager gparted gucharmap luckybackup okular qtqr
+  zeal gnome-software gnome-software-plugin-flatpak gnome-tweaks gnome-boxes)
+
 function remove_if_installed_apt() {
   local package packages=()
   for package; do
@@ -28,41 +37,29 @@ function install_if_available_apt() {
   sudo "$APT" install -y -- "${packages[@]}"
 }
 
-function install_apt_packages() {
-  sudo -v
-  install_if_available_apt age anacron autopoint bat cmake command-not-found curl \
-    dconf-editor duf fwts gh git golang-go grep ifupdown inotify-tools less make \
-    neofetch neovim net-tools openvpn p7zip-full python3-neovim rsync shfmt tlp \
-    trash-cli tree util-linux xclip xdg-utils zip zoxide htop apt-file speedtest-cli \
-    ripgrep apt-clone aptitude libfuse2 hexedit luajit python3-pip ppa-purge xdotool
-}
+function install_apt_packages() { install_if_available_apt "${PACKAGES[@]}"; }
 
 function install_graphical_apt_packages() {
-  sudo -v
   # If gnome is not installed, ask confirmation, else just install
-  if ! (is_installed_apt gnome-shell || confirm "Gnome is not installed, would you still like to install gui applications?"); then
-    return 2
+  if ! is_installed_apt gnome-shell; then
+    confirm "Gnome is not installed, would you still like to install gui applications?" || return 2
   fi
-
-  install_if_available_apt dconf-editor flatpak gimp \
-    gnome-shell-extension-manager gparted gucharmap luckybackup okular qtqr \
-    zeal gnome-software gnome-software-plugin-flatpak gnome-tweaks gnome-boxes
+  install_if_available_apt "${GRAPHICAL_PACKAGES[@]}"
 }
 
-# Only run if apt is available
-installed_or_log apt && {
-  log_and_run 'installing neovim nightly ppa' sudo add-apt-repository -n -y -P ppa:neovim-ppa/unstable ||
-    err "Yeah, that didn't work. Neovim will be installed from default repos."
-  log_and_run 'Updating apt sources' sudo "$APT" update
-  log_and_run 'Installing apt packages' install_apt_packages
-  log_and_run 'Installing graphical (gnome) apt packages' install_graphical_apt_packages
-  # Gnome comes with it, but I don't want it.
-  # Replace the existing cheese package with the flatpak package
-  remove_if_installed_apt gnome-characters cheese
-  # Install vim symlink to nvim
-  if command -v nvim &>/dev/null; then
-    if ! [ "$(basename -- "$(update-alternatives --query vim | grep 'Value: /.\+' | cut -d' ' -f2-)")" = nvim ]; then
-      sudo update-alternatives --install /usr/bin/vim vim "$(which nvim)" 100 || true
-    fi
+# Only run if apt is available - exits if false
+installed_or_log apt
+
+log_and_run 'installing neovim nightly ppa' sudo add-apt-repository -n -y -P ppa:neovim-ppa/unstable
+log_and_run 'Updating apt sources' sudo "$APT" update
+log_and_run 'Installing apt packages' install_apt_packages
+log_and_run 'Installing graphical (gnome) apt packages' install_graphical_apt_packages
+# Gnome comes with it, but I don't want it.
+# Replace the existing cheese package with the flatpak package
+remove_if_installed_apt gnome-characters cheese
+# Install vim symlink to nvim
+if command -v nvim &>/dev/null; then
+  if ! [ "$(basename -- "$(update-alternatives --query vim | grep 'Value: /.\+' | cut -d' ' -f2-)")" = nvim ]; then
+    sudo update-alternatives --install /usr/bin/vim vim "$(which nvim)" 100 || true
   fi
-}
+fi
