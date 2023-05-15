2ee2e7b4232f2e681c8adf10ef0dac1d806a93a1
aarondill <aaronsacks2006@gmail.com>
2023-05-15 00:02:51 -0500

    newlines.
    Former-commit-id: 6cc29ab50715f3cd824bc2dfcfcafdde7713ea40
    Former-commit-id: 250a086c252b9047af6c93cd29a8ca7c953212e1
    Former-commit-id: 6df52695272644b537f2eaefa8ff54d2b4896fdb

--- source/.chezmoiscripts/run_once_before_00-aa-echo-source-path.sh
+++ source/.chezmoiscripts/run_once_before_00-aa-echo-source-path.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euC -o pipefail
-printf "%s = %s" "CHEZMOI_SOURCE_DIR" "$CHEZMOI_SOURCE_DIR"
+printf "%s = %s\n" "CHEZMOI_SOURCE_DIR" "$CHEZMOI_SOURCE_DIR"
 
 chezmoi="${CHEZMOI_EXECUTABLE:-$(which chezmoi 2>/dev/null || printf '')}"
 if [ -x "$chezmoi" ]; then
-  printf "%s = %s" "chezmoi source-path" "$("$chezmoi" source-path)"
+  printf "%s = %s\n" "chezmoi source-path" "$("$chezmoi" source-path)"
 fi
