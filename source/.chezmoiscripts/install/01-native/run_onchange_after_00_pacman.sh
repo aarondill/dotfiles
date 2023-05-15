58781397e03b898132dc523b44a57e05180e6c04
aarondill <aaronsacks2006@gmail.com>
2023-05-15 17:04:55 -0500

    install vscode
    Former-commit-id: 1e84440a41bd54d79b6a7bd4f083057b46de4c42
    Former-commit-id: 2bb57158ec6ce4dfc0e6ad0ca5da5a0cf7d30c26
    Former-commit-id: 8bf829c1fba3b37df7d06de36bd61daff3a46ec2

--- source/.chezmoiscripts/install/01-native/run_onchange_after_00_pacman.sh
+++ source/.chezmoiscripts/install/01-native/run_onchange_after_00_pacman.sh
@@ -21,7 +21,7 @@ PACKAGES=(
 )
 
 GRAPHICAL_PACKAGES=(
-  dconf-editor flatpak gparted gucharmap
+  dconf-editor flatpak gparted gucharmap code
   # VM management. Technically, only virt-manager is graphical, but they are used together
   virt-manager qemu-desktop
   # virsh
