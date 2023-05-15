b7a45e0e0ef902e596138b34f0871ef2f44b1bac
aarondill <aaronsacks2006@gmail.com>
2023-05-14 22:54:00 -0500

    be more quiet on starship install
    Former-commit-id: a20735da92363db142208c2ad24fa5c1348e534d
    Former-commit-id: b72da2d72143cd8b14a32b12411f708bad3137c2
    Former-commit-id: 936182b46c00b42f5f4eb744c972e03b481e3397

--- source/.chezmoiscripts/install/run_onchange_after_34-starship.sh
+++ source/.chezmoiscripts/install/run_onchange_after_34-starship.sh
@@ -2,4 +2,9 @@
 set -euC -o pipefail
 export SHELLOPTS
 # Use the install script! We love this! :)
-curl -sS https://starship.rs/install.sh | sh -s -- -y -b /usr/local/bin
+install_script="$(curl -sSfL https://starship.rs/install.sh)"
+# If ends in print_install, remove it. It's annoying. Be as safe as possible.
+if [ "print_install" = "$(tail -n1 <<<"$install_script")" ]; then
+  install_script=$(head -n1 <<<"$install_script")
+fi
+sh -c "$install_script" -- -y -b /usr/local/bin
