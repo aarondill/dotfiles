52dc1948d442b69646928582dae4d0cd190f8090
aarondill <aaronsacks2006@gmail.com>
2023-05-15 17:04:10 -0500

    install google-chrome from aur
    Former-commit-id: 34a55c0a6905c78e173179d4078c4a398bf9662d
    Former-commit-id: 896db6b7c6b8db8fd64e499c5ffde0a144f343c3
    Former-commit-id: 57b3f0fbdf1e642fc45e02e01df81a7d7fa8983a

--- source/.chezmoiscripts/install/01-native/run_onchange_after_01_arch_aur.sh
+++ source/.chezmoiscripts/install/01-native/run_onchange_after_01_arch_aur.sh
@@ -6,21 +6,29 @@ SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
 # shellcheck source=../../.utils.sh
 . "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
 
-REPO=https://aur.archlinux.org/zeal.git
-
 # Defined in utils.sh
 if [ -z "$PACMAN" ]; then
   exit 0 # Assume already knows
-elif command -v zeal &>/dev/null; then
-  abort 'Zeal is already installed' 0
 fi
 
-# Dependancy that's no longer supplied by pacman
-pacman -U https://archive.archlinux.org/packages/q/qt5-webkit/qt5-webkit-5.212.0alpha4-18-x86_64.pkg.tar.zst
+aur_install() {
+  REPO=$1
+  tmpdir="$(mktemp -d)"
+  trap 'rm -rf "$tmpdir"' EXIT
+  git clone "$REPO" "$tmpdir"
+  cd "$tmpdir"
+  (export -n SHELLOPTS && makepkg -sirc)
+  rm -rf "$tmpdir" && trap '' EXIT
+}
 
-tmpdir="$(mktemp -d)"
-trap 'rm -rf "$tmpdir"' EXIT
-git clone "$REPO" "$tmpdir"
-cd "$tmpdir"
-(export -n SHELLOPTS && makepkg -sirc)
-rm -rf "$tmpdir" && trap '' EXIT
+# Zeal
+if ! command -v zeal &>/dev/null; then
+  # Dependancy that's no longer supplied by pacman
+  pacman -U https://archive.archlinux.org/packages/q/qt5-webkit/qt5-webkit-5.212.0alpha4-18-x86_64.pkg.tar.zst
+  aur_install "https://aur.archlinux.org/zeal.git"
+else err "Zeal is already installed, skipping installation"; fi
+
+# Google chrome
+if ! command -v google-chrome &>/dev/null; then
+  aur_install "https://aur.archlinux.org/google-chrome.git"
+else err "google-chrome is already installed, skipping installation"; fi
