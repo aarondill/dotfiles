b9d628aca429ae405da408bd078a4ed940c53ea2
aarondill <aaronsacks2006@gmail.com>
2023-05-15 08:38:00 -0500

    refactor echo-source-path
    now exits if they don't match
    
    
    Former-commit-id: d724139e0c44fc1a0de861f07734fb0bbc73dcf6
    Former-commit-id: 78ed156a03516b180e58dc6d3ac19bde20d2ad6c
    Former-commit-id: 2d1daa5ba4cbf7a0221730c9c98bbeca4309e3ba

--- source/.chezmoiscripts/run_once_before_00-aa-echo-source-path.sh
+++ source/.chezmoiscripts/run_once_before_00-aa-echo-source-path.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 set -euC -o pipefail
-printf "%s = %s\n" "CHEZMOI_SOURCE_DIR" "$CHEZMOI_SOURCE_DIR"
 
 chezmoi="${CHEZMOI_EXECUTABLE:-$(which chezmoi 2>/dev/null || printf '')}"
-if [ -x "$chezmoi" ]; then
-  printf "%s = %s\n" "chezmoi source-path" "$("$chezmoi" source-path)"
+
+source_path='Could not find chezmoi in PATH'
+[ -x "$chezmoi" ] && source_path="$("$chezmoi" source-path)"
+
+printf "%s = %s\n" "CHEZMOI_SOURCE_DIR" "${CHEZMOI_SOURCE_DIR:-Not Defined}"
+printf "%s = %s\n" "chezmoi source-path" "$source_path"
+
+err() { printf '%s\n' "$@" >&2; }
+abort() {
+  err "$1"
+  exit "${2:-1}"
+}
+# Only when both defined, check whether same. won't break if run explicitly.
+if [ -n "$source_path" ] && [ -n "$CHEZMOI_SOURCE_DIR" ] && [ "$source_path" != "$CHEZMOI_SOURCE_DIR" ]; then
+  err 'chezmoi source-path and CHEZMOI_SOURCE_DIR do not match! This is most likely an error'
+  abort 'This may be fixed by running chezmoi apply again.' 1
 fi
