3afb3582ab149ac39eee73fd573241e60d12e4ff
aarondill <aaronsacks2006@gmail.com>
2023-05-15 02:13:52 -0500

    install zeal on arch
    Former-commit-id: cc2fcf61b1814311bd88efc96f001001c3128757
    Former-commit-id: 5abe6b264b3f13896eb7ec63756cceabe90761fe
    Former-commit-id: 54df56f4ec28d3ed6f6c87e6b1adb628477e2de3

--- source/.chezmoiscripts/install/01-native/run_onchange_after_01_arch_zeal.sh
+++ source/.chezmoiscripts/install/01-native/run_onchange_after_01_arch_zeal.sh
@@ -6,8 +6,18 @@ SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
 # shellcheck source=../../.utils.sh
 . "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
 
+REPO=https://aur.archlinux.org/zeal.git
+
 # Defined in utils.sh
 if [ -z "$PACMAN" ]; then
-  # STOP! deps may not be installed, hopefully handled elsewhere
-  abort0 "pacman is not installed, skipping pacman zeal installation"
+  exit 0 # Assume already knows
+elif command -v zeal &>/dev/null; then
+  abort 'Zeal is already installed' 0
 fi
+
+tmpdir="$(mktemp -d)"
+trap 'rm -rf "$tmpdir"' EXIT
+git clone "$REPO" "$tmpdir"
+cd "$tmpdir"
+makepkg -sirc
+rm -rf "$tmpdir" && trap '' EXIT
