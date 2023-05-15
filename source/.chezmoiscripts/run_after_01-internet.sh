ef93e65c9e33daf2fe4eb2130edda7021203a92d
aarondill <aaronsacks2006@gmail.com>
2023-05-14 21:43:54 -0500

    nvmnd on the internet check, it's anoying
    Former-commit-id: 47f24c07698733ec8f5d4758c73a71a519a8b921
    Former-commit-id: 4e1d997cd17fedb1f07953b354c5a0ba8a0ec524
    Former-commit-id: 448ea8a25346c86992107463d3b755957643db15

--- source/.chezmoiscripts/run_after_01-internet.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euC -o pipefail
-internet="$(which internet 2>/dev/null || printf '%s' "$HOME/.local/bin/internet")"
-# Exit on fail, if it exists
-if [ -x "$internet" ]; then "$internet"; fi
