b621724676468aacc6e18ca53f61db54e70cb8b1
aarondill <aaronsacks2006@gmail.com>
2023-05-15 20:24:06 -0500

    comment?
    Former-commit-id: 0ae0efbe95552090360b85f1e5075871d0458995
    Former-commit-id: 4bbc27963ae3e7cb3059f05f23bea5d5e9f617c9
    Former-commit-id: d53abae827d1217d064b7b0ba9bd842d2c0bf9c1

--- source/.chezmoiscripts/install/run_onchange_after_35-tealdeer.sh
+++ source/.chezmoiscripts/install/run_onchange_after_35-tealdeer.sh
@@ -6,6 +6,7 @@ SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
 # shellcheck source=../.utils.sh
 . "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
 
+# Hard coded, but whatever
 FILE="tealdeer-linux-x86_64-musl"
 DESTINATION=/usr/local/bin/tldr
 REPO='dbrgn/tealdeer'
