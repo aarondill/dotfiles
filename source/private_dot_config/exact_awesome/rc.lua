e78fe457ee6e897b05d7e7d5761f36c9f960d1bc
aarondill <aaronsacks2006@gmail.com>
2023-05-16 11:41:23 -0500

    Update source/.config/awesome/rc.lua
    Former-commit-id: 7f1db33cd0cb35a501b00281ed252208d2081cc5
    Former-commit-id: f975c88be2ee1fd2d153e8398c26d6a7d500277b
    Former-commit-id: 6655a8351c5859c09c842f24b0bb7632658728c3

--- source/private_dot_config/exact_awesome/rc.lua
+++ source/private_dot_config/exact_awesome/rc.lua
@@ -493,6 +493,7 @@ awful.rules.rules = {
 			buttons = clientbuttons,
 			screen = awful.screen.preferred,
 			placement = awful.placement.no_overlap + awful.placement.no_offscreen,
+			size_hint_honor = false,
 		},
 	},
 
