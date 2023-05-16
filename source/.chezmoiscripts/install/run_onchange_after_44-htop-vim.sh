0682eb929e3427d0d31b7118bc633205d208c865
aarondill <aaronsacks2006@gmail.com>
2023-05-16 08:55:31 -0500

    install htop-vim
    Former-commit-id: abeef2d2ee75a5904b9ea00bff4cad5461aa5024
    Former-commit-id: 667736e2ccce942d4080ecde669c9279807687e9
    Former-commit-id: 82e847b4b3a8c4f5792630472975a62eb186cbca

---
+++ source/.chezmoiscripts/install/run_onchange_after_44-htop-vim.sh
@@ -0,0 +1,66 @@
+#! /usr/bin/env bash
+set -euC -o pipefail
+export SHELLOPTS
+# Source utils
+SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
+# shellcheck source=../.utils.sh
+. "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
+
+# dependencies installed on each distribution
+DEBIAN_DEPS=(libncursesw5-dev autotools-dev autoconf automake build-essential)
+ARCH_DEPS=(ncurses automake autoconf gcc)
+# Run in the source directory
+PREHOOK=(sed -i "s/ --verbose\b//g" autogen.sh)
+# Passed to ./configure
+CONFIGURE_ARGS=(--enable-unicode=no --silent)
+# Passed to *all* make invocations
+MAKE_ARGS=(--silent)
+# Cloned into tempdir
+REPO_URL='https://github.com/KoffeinFlummi/htop-vim'
+
+export WARNINGS=none
+
+install_from_make() {
+  # Shows on errors
+  THIS="Make"
+
+  # Try install deps *first*
+  if [ -n "$APT" ]; then
+    if [ "${#DEBIAN_DEPS[@]}" -gt 0 ]; then
+      sudo "$APT" install -y "${DEBIAN_DEPS[@]}"
+    fi
+  elif [ -n "$PACMAN" ]; then
+    if [ "${#ARCH_DEPS[@]}" -gt 0 ]; then
+      sudo "$PACMAN" -S "${ARCH_DEPS[@]}"
+    fi
+  else
+    err "Unable to install dependencies to build '$REPO_URL'"
+    err "Please install the following dependencies (names are from debian repositories):"
+    err "${DEBIAN_DEPS[@]}"
+    exit 2
+  fi
+
+  # Temp directory
+  temp=$(mktemp -d)
+  # trap 'rm -rf "$temp"' EXIT
+
+  # Clone to tempdir
+  git clone --quiet -- "$REPO_URL" "$temp" >/dev/null
+
+  # build from source
+  cd "$temp"
+  "${PREHOOK[@]}"
+  (
+    export -n SHELLOPTS # Let shells set their own options
+    ./autogen.sh
+    ./configure "${CONFIGURE_ARGS[@]}"
+    make "${MAKE_ARGS[@]}"
+    # Install the updated application
+    make "${MAKE_ARGS[@]}" install
+  )
+
+  # cleanup
+  # rm -rf "$temp" && trap '' EXIT
+}
+
+log_and_run "Installing $REPO_URL" install_from_make
