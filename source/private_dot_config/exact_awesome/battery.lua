2d44eebd2b7388256c17547c29eebb88f467d5ea
aarondill <aaronsacks2006@gmail.com>
2023-05-17 07:37:42 -0500

    Update source/.config/awesome/battery.lua
    Former-commit-id: 8cf9180e71fb9e7fea7aaca911d9fd718e1108e3
    Former-commit-id: 3cc5f8dc59f5e7bcab85550356fcc3a983ffc0c5
    Former-commit-id: f8c1eeed01af68cea44c18df191bf4662de30ac4

--- source/private_dot_config/exact_awesome/battery.lua
+++ source/private_dot_config/exact_awesome/battery.lua
@@ -6,8 +6,7 @@ local wibox = require("wibox")
 
 -- Create a widget and update its content using the output of a shell
 -- command every 10 seconds:
----@param args table? ununsed
-local function batterybar(args)
+local function batterybar()
 	local batbar = wibox.widget({
 		{ id = "batbar", text = "bat: unknown", widget = wibox.widget.textbox },
 		layout = wibox.layout.stack,
