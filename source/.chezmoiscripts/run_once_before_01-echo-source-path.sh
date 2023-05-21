0ed209cdb3e4f6a4448d8bfc6bd82e37458b592f
aarondill <aaronsacks2006@gmail.com>
2023-05-20 23:05:19 -0500

    cleanup echo-source-path a bit
    Former-commit-id: 7122843baf9b9653137bb6d14c1cd1d97012cc7b
    Former-commit-id: 005884cd4559309211f5a5afff4987343bb05e28
    Former-commit-id: 2d1d5b9159461e611a43641a0465f52864710ad3

--- source/.chezmoiscripts/run_once_before_01-echo-source-path.sh
+++ source/.chezmoiscripts/run_once_before_01-echo-source-path.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euC -o pipefail
+err() { printf '%s\n' "$@" >&2; }
 
 chezmoi="${CHEZMOI_EXECUTABLE:-$(which chezmoi 2>/dev/null || printf '')}"
 
@@ -9,13 +10,9 @@ source_path='Could not find chezmoi in PATH'
 printf "%s = %s\n" "CHEZMOI_SOURCE_DIR" "${CHEZMOI_SOURCE_DIR:-Not Defined}"
 printf "%s = %s\n" "chezmoi source-path" "$source_path"
 
-err() { printf '%s\n' "$@" >&2; }
-abort() {
-  err "$1"
-  exit "${2:-1}"
-}
 # Only when both defined, check whether same. won't break if run explicitly.
 if [ -n "$source_path" ] && [ -n "$CHEZMOI_SOURCE_DIR" ] && [ "$source_path" != "$CHEZMOI_SOURCE_DIR" ]; then
   err 'chezmoi source-path and CHEZMOI_SOURCE_DIR do not match! This is most likely an error'
-  abort 'This may be fixed by running chezmoi apply again.' 1
+  err 'This may be fixed by running chezmoi apply again.'
+  exit 1
 fi
