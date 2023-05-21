7dfe9c51f7a98c70ee88a29cd5c9e0ac081e180f
aarondill <aaronsacks2006@gmail.com>
2023-05-20 23:09:16 -0500

    print newline when waiting for key
    Former-commit-id: 82e9d458416e4494e925fef8811b2b0e74ccdd1c
    Former-commit-id: 57eb2429f15623a9f3b6ef069bc6d68388497b64
    Former-commit-id: 5f2890b0cfd34c8b91a4539a81152dc1a17e5897

--- source/.chezmoiscripts/run_before_00_check_stdin.sh
+++ source/.chezmoiscripts/run_before_00_check_stdin.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-if ! [ -t 0 ] || ! read -n1 -s -r -p "Checking stdin safety. Press any key to continue."; then
+wait_key() {
+  local code=0
+  read -n1 -s -r -p "Checking stdin safety. Press any key to continue." || code=$?
+  printf '\n'
+  return "$code"
+}
+if ! [ -t 0 ] || ! wait_key; then
   printf '%s\n' "Don't run this in a pipe. Stdin must be accessible to the terminal for prompts." >&2
   exit 1
 fi
