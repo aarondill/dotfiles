78ea91bea631637de914a4a37749640e08e36452
aarondill <aaronsacks2006@gmail.com>
2023-05-31 23:49:34 -0500

    Update source/.local/bin/script_utils.sh
    Former-commit-id: 33f3ef3981edf28a392f23803148f8210ff22f9a
    Former-commit-id: 5556f4f548cf04aa692cd675b31baf7c152d8ecf
    Former-commit-id: d12fb51f60493372cf8bbf9c1a2ea15c47509451

--- source/dot_local/exact_bin/executable_script_utils.sh
+++ source/dot_local/exact_bin/executable_script_utils.sh
@@ -23,7 +23,7 @@ EOF
 }
 
 log() { printf '%s\n' "$@"; }
-err() { printf "${THIS:}: %s\n" "$@" >&2; }
+err() { printf "${THIS:+$THIS: }%s\n" "$@" >&2; }
 abort() { err "$1" && exit "${2:-1}"; }
 
 # Returns a string that should be 'eval'ed to set the positional arguments
