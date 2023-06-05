7367b95547938742a9fc0a11299a72e1478173fc
aarondill <aaronsacks2006@gmail.com>
2023-06-05 05:08:19 -0500

    Add source/.chezmoiscripts/install/46-icons-and-themes.sh
    Former-commit-id: f047fd7774a877b86a2bc3f0531c279d32fb9b7c
    Former-commit-id: 37ee1429849b51c45b289879ae2be038117d149f
    Former-commit-id: 8863376af52a40acfeb5c8baca17f2ef728ccf82

---
+++ source/.chezmoiscripts/install/run_onchange_after_46-icons-and-themes.sh
@@ -0,0 +1,52 @@
+#! /usr/bin/env bash
+set -euC -o pipefail
+export SHELLOPTS
+# Source utils
+SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
+# shellcheck source=../.utils.sh
+. "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
+
+function install_humanity_icons() {
+  local TMP_DIR
+
+  TMP_DIR=$(mktemp -d)
+  trap 'rm -rf "$TMP_DIR"' EXIT
+
+  git clone --single-branch --branch=ubuntu/devel 'https://git.launchpad.net/ubuntu/+source/humanity-icon-theme' "$TMP_DIR"
+  sudo mv -vi "$TMP_DIR/Humanity" "$TMP_DIR/Humanity-Dark" "/usr/share/icons"
+
+  rm -rf "$TMP_DIR" && trap '' EXIT # cleanup
+}
+
+function install_yaru() (
+  local TMP_DIR
+
+  TMP_DIR=$(mktemp -d)
+  trap 'rm -rf "$TMP_DIR"' EXIT
+  if [ -n "$APT" ]; then
+    sudo "$APT" install libgtk-3-dev git meson sassc
+  elif [ -n "$PACMAN" ]; then
+    sudo "$PACMAN" -S libgtk-3-dev git meson sassc
+  else
+    abort "Could not install dependencies for yaru theme. Please install it yourself manually" 0
+  fi
+
+  git clone --filter=tree:0 --single-branch https://github.com/ubuntu/yaru.git "$TMP_DIR"
+
+  cd "$TMP_DIR"
+
+  meson build # requires meson >=0.59!
+  cd build
+
+  sudo ninja install
+
+  rm -rf "$TMP_DIR" && trap '' EXIT # cleanup
+)
+
+if ! [ -d /usr/share/icons/Humanity/ ] || ! [ -d /usr/share/icons/Humanity-Dark/ ]; then
+  log_and_run "Installing Humanity icon theme" install_humanity_icons
+fi
+
+if ! [ -d /usr/share/icons/Yaru/ ] || ! [ -d /usr/share/icons/Yaru-dark/ ]; then
+  log_and_run "Installing Yaru theme" install_yaru
+fi
