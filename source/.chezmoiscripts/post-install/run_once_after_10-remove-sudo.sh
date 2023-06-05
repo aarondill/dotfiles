385e26f629344d4d033e6572ac942f55a31b6f6e
aarondill <aaronsacks2006@gmail.com>
2023-06-05 01:01:35 -0500

    Update source/.chezmoiscripts/post-install/10-remove-sudo.sh Update source/.root/etc/X11/xorg.conf.d/20-intel-gpu.conf
    Former-commit-id: 79ef86030c2be0adde1d4024ec8190b2ebdfafcd
    Former-commit-id: 49b5269df6ef70d061cd0d5f90f9daf371a63b9c
    Former-commit-id: 9b51a2682d01608e249d236610488083a9944129

--- source/.chezmoiscripts/post-install/run_once_after_10-remove-sudo.sh
+++ source/.chezmoiscripts/post-install/run_once_after_10-remove-sudo.sh
@@ -3,9 +3,10 @@
 set -euC -o pipefail
 
 if sudo sudo -V | grep -q -- --enable-admin-flag; then
-  cat <<'EOF' >/etc/sudoers.d/disable_admin_file_in_home
+  cat <<'EOF' | sudo tee /etc/sudoers.d/disable_admin_file_in_home >/dev/null
 # Disable ~/.sudo_as_admin_successful file
 Defaults !admin_flag
 EOF
 fi
+
 sudo sed '/sudo hint/,/To run a command/s/cat <<-EOF/true <<-EOF/' /etc/bash.bashrc -i
