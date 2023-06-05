147951116293992ae5649ac7ea0dc58ad871dc41
aarondill <aaronsacks2006@gmail.com>
2023-06-05 00:59:27 -0500

    Update source/.chezmoiscripts/post-install/10-remove-sudo.sh Remove source/.root/etc/sudoers.d/disable_admin_file_in_home
    Former-commit-id: 960ac987b72fe63341b27601c7cf14fd50e76997
    Former-commit-id: 8c4a86c2b739d035730c01afdc45bceaae305185
    Former-commit-id: ad3e7733490ec8820585208346cf68d358f3aed2

--- source/.chezmoiscripts/post-install/run_once_after_10-remove-sudo.sh
+++ source/.chezmoiscripts/post-install/run_once_after_10-remove-sudo.sh
@@ -2,4 +2,10 @@
 # Remove the "to run a command using sudo..." at start up.
 set -euC -o pipefail
 
+if sudo sudo -V | grep -q -- --enable-admin-flag; then
+  cat <<'EOF' >/etc/sudoers.d/disable_admin_file_in_home
+# Disable ~/.sudo_as_admin_successful file
+Defaults !admin_flag
+EOF
+fi
 sudo sed '/sudo hint/,/To run a command/s/cat <<-EOF/true <<-EOF/' /etc/bash.bashrc -i
