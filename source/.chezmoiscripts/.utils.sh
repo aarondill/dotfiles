d5d3ef01f8f6b46a9d7114d74465e5128cefbb56
aarondill <aaronsacks2006@gmail.com>
2023-06-05 05:12:17 -0500

    add a compare_version function to .utils
    Former-commit-id: 2c51e9b120cf47d82dfd409b2f3b958f92ca90f4
    Former-commit-id: f20f86097754c1274bc458088db5de34b04ff647
    Former-commit-id: cae48116e1a7dfd2dba1a3e3698eb5d30d93b5cb

--- source/.chezmoiscripts/.utils.sh
+++ source/.chezmoiscripts/.utils.sh
@@ -114,6 +114,13 @@ function first_lower() { local t && t="$(cat -)" && printf '%s' "${t,}"; }
 function upper() { local t && t="$(cat -)" && printf '%s' "${t^^}"; }
 # first_upper <<<"hello" -> "Hello"
 function first_upper() { local t && t="$(cat -)" && printf '%s' "${t^}"; }
+# requires sort -V to sort version strings. Errors if $1 is before $2
+function compare_version() {
+  local hasV=$1 ExpecV=$2 versions=
+  versions="$(printf '%s\n' "$hasV" "$ExpecV")"
+  # shellcheck disable=SC2319 # $? *should* refer to the condition, not the sort command
+  test "$versions" != "$(sort -V <<<"$versions")" || exit "$?"
+}
 
 ## --------------------------------------------------------------------------------------------------
 ## --------------------------------------------- GitHub ---------------------------------------------
