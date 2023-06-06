c65b178c6a79ca19f5952479a247990492bf2414
aarondill <aaronsacks2006@gmail.com>
2023-06-06 13:21:14 -0500

    add logs about starship
    Former-commit-id: 6c589b8795d2139faa0b39f535216853aa8dfe51
    Former-commit-id: caedf95cf29df1af365a165f423ba078360a9b4e
    Former-commit-id: a313d34cb59744dd0e466e2f4b14f7d04b32dfa0

--- source/.chezmoiscripts/install/run_onchange_after_34-starship.sh
+++ source/.chezmoiscripts/install/run_onchange_after_34-starship.sh
@@ -1,10 +1,17 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
 set -euC -o pipefail
 export SHELLOPTS
+# Source utils
+SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
+# shellcheck source=../.utils.sh
+. "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
+
 # Use the install script! We love this! :)
 install_script="$(curl -sSfL https://starship.rs/install.sh)"
 # If ends in print_install, remove it. It's annoying. Be as safe as possible.
 if [ "print_install" = "$(tail -n1 <<<"$install_script")" ]; then
   install_script=$(head -n1 <<<"$install_script")
 fi
+log "Installing starship from $install_script"
 sh -c "$install_script" -- -y -b /usr/local/bin
+success
