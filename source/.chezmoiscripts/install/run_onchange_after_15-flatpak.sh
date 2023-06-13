19fa3cd382da71fb4f0c71c7f24e1665aef9b015
aarondill <aaronsacks2006@gmail.com>
2023-06-13 04:03:38 -0500

    remove non-interactive to allow updating system flatpak installs
    Former-commit-id: 51e97197c0200d8107136281351366fdc12500a8
    Former-commit-id: 69c8448d31fe9b832ef33df538f1c0c5204d9188
    Former-commit-id: ccc0787a116cfb4f5f93758b1cd9b5f38c6916f1

--- source/.chezmoiscripts/install/run_onchange_after_15-flatpak.sh
+++ source/.chezmoiscripts/install/run_onchange_after_15-flatpak.sh
@@ -8,9 +8,9 @@ SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
 
 function flatpak_is_installed() { flatpak info -- "$@" &>/dev/null; }
 # usage: flatpak_install [source] package
-function flatpak_install() { flatpak install -y --noninteractive -- "$@"; }
+function flatpak_install() { flatpak install -y -- "$@"; }
 # shellcheck disable=SC2120 # I know it doesn't recieve arguments. it updates all without arguments.
-function flatpak_update() { flatpak update -y --noninteractive -- "$@"; }
+function flatpak_update() { flatpak update -y -- "$@"; }
 
 function install_flatpaks() {
   trap 'err Aborting; exit 1' SIGINT
