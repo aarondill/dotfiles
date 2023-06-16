aa32b2419a6d57446b4a1c6879cbaa8009b1e73c
aarondill <aaronsacks2006@gmail.com>
2023-06-16 02:55:31 -0500

    fix undefined CHEZMOI_SOURCE_DIR
    Former-commit-id: b8489941c871e15a7349b6984f2bbb266758ac66
    Former-commit-id: 269aa4aaf25783f563d65205df751501cf8ec1dd
    Former-commit-id: 9deb99ab41b64b8084ce201307cc9b3ff840b90a

--- source/.chezmoiscripts/run_once_before_01-echo-source-path.sh
+++ source/.chezmoiscripts/run_once_before_01-echo-source-path.sh
@@ -11,7 +11,7 @@ printf "%s = %s\n" "CHEZMOI_SOURCE_DIR" "${CHEZMOI_SOURCE_DIR:-Not Defined}"
 printf "%s = %s\n" "chezmoi source-path" "$source_path"
 
 # Only when both defined, check whether same. won't break if run explicitly.
-if [ -n "$source_path" ] && [ -n "$CHEZMOI_SOURCE_DIR" ] && [ "$source_path" != "$CHEZMOI_SOURCE_DIR" ]; then
+if [ -n "$source_path" ] && [ -n "${CHEZMOI_SOURCE_DIR:-}" ] && [ "$source_path" != "${CHEZMOI_SOURCE_DIR:-}" ]; then
   err 'chezmoi source-path and CHEZMOI_SOURCE_DIR do not match! This is most likely an error'
   err 'This may be fixed by running chezmoi apply again.'
   exit 1
