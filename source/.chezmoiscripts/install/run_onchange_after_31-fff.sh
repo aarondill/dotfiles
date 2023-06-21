f22197bb42e2a5f152ebe6af098d99133627ac3a
aarondill <aaronsacks2006@gmail.com>
2023-06-21 15:56:19 -0500

    remove obsolite sudo argument to download_file
    Former-commit-id: c302808107b9fcfe4c94c0891a8faef353061628
    Former-commit-id: d48fffecd3272ea05adcb610a61acd0b895a7461
    Former-commit-id: bbaef76337554bed16f279748dfc709fdf8bb3a6

--- source/.chezmoiscripts/install/run_onchange_after_31-fff.sh
+++ source/.chezmoiscripts/install/run_onchange_after_31-fff.sh
@@ -9,7 +9,7 @@ SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
 function install_fff() {
   local DEST=${BINDIR:-'/usr/local/bin'}/fff
   local url='https://raw.githubusercontent.com/dylanaraps/fff/master/fff'
-  download_file sudo "$url" "$DEST" 755 # Download file to bindir with rwxr-xr-x perms
+  download_file "$url" "$DEST" 755 # Download file to bindir with rwxr-xr-x perms
 }
 
 log_and_run 'installing fff' install_fff
