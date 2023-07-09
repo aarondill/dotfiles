240cf7f9b5ee6371e2e892ee560d5073536c80fc
aarondill <aaronsacks2006@gmail.com>
2023-07-09 01:44:41 -0500

    Update source/vscode/.prettierrc.cjs

--- source/vscode/dot_prettierrc.cjs
+++ source/vscode/dot_prettierrc.cjs
@@ -1,14 +1,15 @@
 // ~/vscode/.prettierrc.js
 module.exports = {
-	arrowParens: 'avoid',
+	arrowParens: "avoid",
 	bracketSameLine: true,
 	bracketSpacing: true,
-	embeddedLanguageFormatting: 'auto',
-	quoteProps: 'as-needed',
-	endOfLine: 'lf',
+	embeddedLanguageFormatting: "auto",
+	quoteProps: "as-needed",
+	endOfLine: "lf",
 	semi: true,
 	singleAttributePerLine: false,
 	singleQuote: false,
-	trailingComma: 'es5',
-	useTabs: true,
+	trailingComma: "es5",
+	useTabs: false,
+	tabWidth: 2,
 };
