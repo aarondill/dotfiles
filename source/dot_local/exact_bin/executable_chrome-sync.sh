033325746d710fc1d03a826e652552cc9a7f4bde
aarondill <aaronsacks2006@gmail.com>
2023-07-10 03:48:39 -0500

    Update source/.local/bin/chrome-sync.sh

--- source/dot_local/exact_bin/executable_chrome-sync.sh
+++ source/dot_local/exact_bin/executable_chrome-sync.sh
@@ -1,26 +1,36 @@
 #!/bin/sh
+# Syncs the google-chrome assets to /dev/shm to decrease disk writes and improve speeds
+# This should be run before and after google-chrome if possible
 
-static="static-google-chrome/"
-link="google-chrome"
-volatile="/dev/shm/google-chrome-$USER"
+sync_shm() {
+  static="static-$1/"
+  link="$1"
+  volatile="/dev/shm/$1-$USER"
 
-IFS=
-set -efu
+  IFS=
+  set -efu
 
+  if [ ! -r "$volatile" ]; then
+    mkdir -m0700 "$volatile"
+  fi
+
+  if [ "$(readlink "$link")" != "$volatile" ]; then
+    mv "$link" "$static"
+    ln -s "$volatile" "$link"
+  fi
+
+  if [ -e "$link/.unpacked" ]; then
+    rsync -av --delete --exclude .unpacked "./$link/" "./$static/"
+  else
+    rsync -av "./$static/" "./$link/"
+    touch "$link/.unpacked"
+  fi
+}
+
+# PROFILE
 cd ~/.config/
+sync_shm "google-chrome"
 
-if [ ! -r "$volatile" ]; then
-  mkdir -m0700 "$volatile"
-fi
-
-if [ "$(readlink "$link")" != "$volatile" ]; then
-  mv "$link" "$static"
-  ln -s "$volatile" "$link"
-fi
-
-if [ -e "$link/.unpacked" ]; then
-  rsync -av --delete --exclude .unpacked "./$link/" "./$static/"
-else
-  rsync -av "./$static/" "./$link/"
-  touch "$link/.unpacked"
-fi
+# CACHE
+cd ~/.cache/
+sync_shm "google-chrome"
