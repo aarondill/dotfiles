0ebfaaba49d134a817704fc09be32bf312d5668a
aarondill <aaronsacks2006@gmail.com>
2023-07-10 04:18:09 -0500

    Update source/.local/bin/chrome-sync.sh

--- source/dot_local/exact_bin/executable_chrome-sync.sh
+++ source/dot_local/exact_bin/executable_chrome-sync.sh
@@ -2,6 +2,10 @@
 set -eu
 # Syncs the google-chrome assets to /dev/shm to decrease disk writes and improve speeds
 # This should be run before and after google-chrome if possible
+if ! command -v rsync >/dev/null 2>&1; then
+  printf '%s\n' "Rsync is required to run $0" >&2
+  exit 1
+fi
 
 sync_shm() {
   static="static-$1/"
@@ -12,19 +16,23 @@ sync_shm() {
   set -efu
 
   if [ ! -r "$volatile" ]; then
-    mkdir -m0700 "$volatile"
+    mkdir -m0700 -- "$volatile"
+  fi
+
+  if ! [ -e "$link" ] && ! [ -f "$link" ]; then
+    mkdir -- "$link"
   fi
 
   if [ "$(readlink "$link")" != "$volatile" ]; then
-    mv "$link" "$static"
-    ln -s "$volatile" "$link"
+    mv -- "$link" "$static"
+    ln -s -- "$volatile" "$link"
   fi
 
   if [ -e "$link/.unpacked" ]; then
     rsync -av --delete --exclude .unpacked "./$link/" "./$static/"
   else
     rsync -av "./$static/" "./$link/"
-    touch "$link/.unpacked"
+    touch -- "$link/.unpacked"
   fi
 }
 
