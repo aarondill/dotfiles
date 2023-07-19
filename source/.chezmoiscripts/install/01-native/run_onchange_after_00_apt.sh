cc4cffb66f859ac133ae37c2e9869ef1da4ea0e1
aarondill <aaronsacks2006@gmail.com>
2023-07-19 03:21:41 -0500

    don't install fd, because it's installed through pnpm

--- source/.chezmoiscripts/install/01-native/run_onchange_after_00_apt.sh
+++ source/.chezmoiscripts/install/01-native/run_onchange_after_00_apt.sh
@@ -17,7 +17,7 @@ PACKAGES=(
   dconf-editor duf gh git grep inotify-tools less make
   neofetch neovim net-tools openvpn p7zip-full rsync tlp
   trash-cli tree util-linux zip zoxide speedtest-cli
-  ripgrep libfuse2 hexedit luajit python3-pip fd-find
+  ripgrep libfuse2 hexedit luajit python3-pip
   # fonts
   fonts-noto-cjk fonts-noto-mono
   # icon fonts
--- source/.chezmoiscripts/install/01-native/run_onchange_after_00_pacman.sh
+++ source/.chezmoiscripts/install/01-native/run_onchange_after_00_pacman.sh
@@ -17,7 +17,7 @@ PACKAGES=(
   grep inotify-tools less make neofetch neovim net-tools
   openvpn p7zip rsync shfmt tlp trash-cli tree util-linux
   zip unzip zoxide speedtest-cli ripgrep hexedit fuse2
-  luajit python-pip base-devel wezterm bat bash-completion fd
+  luajit python-pip base-devel wezterm bat bash-completion
   # fonts
   ttf-nerd-fonts-symbols-mono noto-fonts-cjk noto-fonts-extra
   # icon fonts
