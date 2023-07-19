fe576050354beb9841eb9f2219ba57ea48e4ebca
aarondill <aaronsacks2006@gmail.com>
2023-07-19 05:27:48 -0500

    Update source/.local/bin/chrome-sync.sh

--- source/dot_local/exact_bin/executable_chrome-sync.sh
+++ source/dot_local/exact_bin/executable_chrome-sync.sh
@@ -10,7 +10,7 @@ fi
 sync_shm() {
   static="static-$1/"
   link="${1%/}"
-  volatile="/dev/shm/$1-$USER"
+  volatile="/dev/shm/$2-$USER"
 
   IFS=
   set -efu
@@ -39,9 +39,9 @@ sync_shm() {
 # PROFILE
 conf_dir=${CHROME_CONFIG_HOME:-${XDG_CONFIG_HOME:-$HOME/.config}}
 cd "$conf_dir"
-sync_shm "google-chrome"
+sync_shm "google-chrome" 'google-chrome-config'
 
 # CACHE
 cache_dir=${XDG_CACHE_HOME:-$HOME/.cache}
 cd "$cache_dir"
-sync_shm "google-chrome"
+sync_shm "google-chrome" "google-chrome-cache"
