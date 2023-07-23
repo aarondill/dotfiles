1f1ae2ed7a18a5d5053716d935248fa84da5e048
aarondill <aaronsacks2006@gmail.com>
2023-07-23 06:22:46 -0500

    refactor lazygit; up to date

--- source/.chezmoiscripts/install/run_onchange_after_33-lazygit.sh
+++ source/.chezmoiscripts/install/run_onchange_after_33-lazygit.sh
@@ -7,23 +7,27 @@ SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
 . "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
 
 function install_lazygit() (
-  local LAZYGIT_VERSION REPO FILE
-  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-  FILE="lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-  BINDIR=/usr/local/bin
-  REPO='jesseduffield/lazygit'
+  local version file
+  local BINDIR=/usr/local/bin
+  local REPO='jesseduffield/lazygit'
+  version=$(get_latest_version_github "$REPO") # v0.39.3
+  file="lazygit_${version#v}_Linux_x86_64.tar.gz"
+  if is_accessible_cmd lazygit; then
+    local installed_version
+    installed_version=$(lazygit --version | tr ',' '\n' | sed 's/^\s*//g' | grep '^version=' | cut -d= -f2-)
+    if [ "$installed_version" = "$version" ]; then
+      abort 'Already up to date' 0
+    fi
+  fi
 
-  log_github_install "$REPO" "$LAZYGIT_VERSION" "$FILE" "$BINDIR"
-
-  TMP=$(mktemp)
-  trap 'rm -f "$TMP"' EXIT
-
-  curl -SsLf "https://github.com/$REPO/releases/download/v$LAZYGIT_VERSION/$FILE" -o "$TMP"
+  tmp=$(mktemp)
+  trap 'rm -f "$tmp"' EXIT
+  install_from_github "$REPO" "$version" "$file" "$tmp"
   # output to destination
-  sudo tar -xvz -C "$BINDIR" -f "$TMP" lazygit
+  sudo tar -xvz -C "$BINDIR" -f "$tmp" lazygit
   sudo chmod +x "$BINDIR/lazygit"
 
-  rm -f "$TMP" && trap '' EXIT # Cleanup
+  rm -f "$tmp" && trap '' EXIT # Cleanup
 )
 
 log_and_run 'installing lazygit' install_lazygit
