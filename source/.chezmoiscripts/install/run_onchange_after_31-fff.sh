191b701799ebbd0f5e162ff357e974b2dbcb12c6
aarondill <aaronsacks2006@gmail.com>
2023-07-23 06:22:23 -0500

    install fff man page

--- source/.chezmoiscripts/install/run_onchange_after_31-fff.sh
+++ source/.chezmoiscripts/install/run_onchange_after_31-fff.sh
@@ -7,9 +7,15 @@ SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
 . "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
 
 function install_fff() {
-  local DEST=${BINDIR:-'/usr/local/bin'}/fff
+  local DEST=/usr/local/bin/fff
   local url='https://raw.githubusercontent.com/dylanaraps/fff/master/fff'
   download_file "$url" "$DEST" 755 # Download file to bindir with rwxr-xr-x perms
 }
+function install_fff_man() {
+  local DEST=/usr/share/man/man1/fff.1
+  local url='https://raw.githubusercontent.com/dylanaraps/fff/master/fff.1'
+  download_file "$url" "$DEST" # Download file to mandir with rwxr-xr-x perms
+}
 
 log_and_run 'installing fff' install_fff
+log_and_run 'installing fff man' install_fff_man
