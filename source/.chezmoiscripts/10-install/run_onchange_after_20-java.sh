a63cfa883e6c3b67276661fc420cdda003ca0dc2
aarondill <aaronsacks2006@gmail.com>
2023-08-03 06:44:24 -0500

    update java download

--- source/.chezmoiscripts/10-install/run_onchange_after_20-java.sh
+++ source/.chezmoiscripts/10-install/run_onchange_after_20-java.sh
@@ -6,24 +6,21 @@ SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
 # shellcheck source=../.utils.sh
 . "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
 
-DESTINATION="/usr/lib/jvm" # Fixed location
-VERSION=20.0.2
-URL=https://download.java.net/java/GA/jdk20.0.2/6e380f22cbe7469fa75fb448bd903d8e/9/GPL/openjdk-20.0.2_linux-x64_bin.tar.gz
+destination="/usr/lib/jvm" # Fixed location
+version=20.0.2
+url=https://download.java.net/java/GA/jdk20.0.2/6e380f22cbe7469fa75fb448bd903d8e/9/GPL/openjdk-20.0.2_linux-x64_bin.tar.gz
 
-if [ -x "$DESTINATION/jdk-$VERSION/bin/java" ]; then
-  abort "jdk-$VERSION is already installed." 0
+if [ -x "$destination/jdk-$version/bin/java" ]; then
+  abort "jdk-$version is already installed." 0
 fi
 
-log "downloading jdk-$VERSION (fixed version)"
+log "downloading jdk-$version (fixed version)"
 
-TMP=$(mktemp)
-trap 'rm -f "$TMP"' EXIT
-
-curl -SsLf "$URL" -o "$TMP"
+tmp=$(download_file "$url")
+rm_exit "$tmp"
 # output to destination
-sudo mkdir -p "$DESTINATION"
-sudo tar -xz -C "$DESTINATION" -f "$TMP"
-
-rm -f "$TMP" && trap '' EXIT # Cleanup
+sudo mkdir -p "$destination"
+sudo tar -xz -C "$destination" -f "$tmp"
+rm_exit_cleanup "$tmp"
 # This is my own script! should be in ~/.local/bin/update-java!
-sudo ~/.local/bin/update-java --quiet --yes "$DESTINATION/jdk-$VERSION"
+sudo ~/.local/bin/update-java --quiet --yes "$destination/jdk-$version"
