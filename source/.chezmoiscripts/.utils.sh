ff19d230698d19c1ec7362e3bfe6dfd6b0b7b789
aarondill <aaronsacks2006@gmail.com>
2023-08-03 10:12:51 -0500

    apt utils work with multiple args

--- source/.chezmoiscripts/.utils.sh
+++ source/.chezmoiscripts/.utils.sh
@@ -301,17 +301,23 @@ function log_github_install() {
 ## ------------------------------------------- APT utils --------------------------------------------
 ## --------------------------------------------------------------------------------------------------
 function apt_is_installed() { dpkg -s "$@" &>/dev/null; }
-function apt_is_available() { test -n "$(apt-cache show -- "$1" 2>/dev/null)"; }
+function apt_is_available() { for pac; do test -n "$(apt-cache show -- "$pac" 2>/dev/null)" || return 1; done; }
 # usage: apt_install file_or_package
-function apt_install() { sudo "$APT" install -y -- "$@"; }
+function apt_install() { sudo "$APT" install -- "$@"; }
 # Just updates, not upgrade - upgrade shouldn't be necessary, leave that to the user.
 function apt_update() { sudo "$APT" update; }
-function apt_is_held() { [ -n "$(apt-mark showhold -- "$1")" ]; }
+function apt_is_held() { for pac; do [ -n "$(apt-mark showhold -- "$pac")" ] || return 1; done; }
+function apt_remove() { sudo "$APT" purge -- "$@"; } # purge for symmetry with pacman. Be careful!
 
 ## --------------------------------------------------------------------------------------------------
 ## ----------------------------------------- Pacman utils -------------------------------------------
 ## --------------------------------------------------------------------------------------------------
 
+# Internal function. don't call.
+_pacman_exec() (
+  export -n SHELLOPTS # makepkg/yay doesn't play nice with this
+  "$@"
+)
 # treat yay like pacman (no sudo/--repo)
 function pacyay() {
   args=("$@")
@@ -319,13 +325,19 @@ function pacyay() {
   */yay) args=("$PACMAN" --repo "${args[@]}") ;;
   *) args=(sudo "$PACMAN" "${args[@]}") ;;
   esac
-  "${args[@]}"
+  _pacman_exec "${args[@]}"
 }
-function pacman_is_installed() { pacyay -Qi "$@" &>/dev/null || return 1; }
-function pacman_is_available() { pacyay -Si "$1" &>/dev/null || return 1; }
+function pacman_is_installed() { for pac; do pacyay -Q -- "$pac" &>/dev/null || return 1; done; }
+function pacman_is_available() { for pac; do pacyay -Si -- "$pac" &>/dev/null || return 1; done; }
 # usage: pacman_install package
 function pacman_install() { pacyay -S --needed -- "$@"; }
 function pacman_update() { pacyay -Syu; }
+function pacman_remove() { pacyay -Rncus -- "$@"; } # Careful here!
+# install aur packages. not repo. use pacman_* for that.
+function yay_install() {
+  has_cmd yay || abort "Make sure yay is installed before using yay_install." 127
+  _pacman_exec yay --aur -S --needed -- "$@"
+}
 
 ## --------------------------------------------------------------------------------------------------
 ## ------------------------------------------- Variables --------------------------------------------
