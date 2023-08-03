30b97ec05148bfa696543e56acb2aca01ddf1a8d
aarondill <aaronsacks2006@gmail.com>
2023-08-03 06:04:59 -0500

    update bat script

--- source/.chezmoiscripts/10-install/00-debian/run_onchange_after_20-bat.sh
+++ source/.chezmoiscripts/10-install/00-debian/run_onchange_after_20-bat.sh
@@ -3,7 +3,7 @@ set -euC -o pipefail
 export SHELLOPTS
 # Source utils
 SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
-# shellcheck source=../.utils.sh
+# shellcheck source=../../.utils.sh
 . "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
 
 log 'installing bat'
@@ -14,10 +14,10 @@ if [ -z "$APT" ]; then
   abort "This script only supports debian-based distos. install manually from $REPO" 0
 fi
 
-VERSION=$(get_latest_version_github "$REPO") # v0.23.0
+version=$(get_latest_version_github "$REPO") # v0.23.0
 if is_accessible_cmd dpkg-query; then
   installed_version=$(dpkg-query --showformat='${Version}' --show bat-musl)
-  if [ "v$installed_version" = "$VERSION" ]; then
+  if [ "v$installed_version" = "$version" ]; then
     abort 'Already up to date' 0
   fi
 fi
@@ -28,14 +28,10 @@ x86_64) arch=amd64 ;;
 i386 | i686) arch=i686 ;;
 esac
 
-ASSET="bat-musl_${VERSION#v}_${arch}.deb"
+asset="bat-musl_${version#v}_${arch}.deb"
 
-TMP_DIR=$(mktemp -d)
-trap 'rm -rf "$TMP_DIR"' EXIT
-DESTINATION=$TMP_DIR/bat.deb
-
-log_github_install "$REPO" "$VERSION" "$ASSET" "$DESTINATION"
-curl -sSL "https://github.com/$REPO/releases/download/$VERSION/$ASSET" -o "$DESTINATION"
-sudo "$APT" install -y "$DESTINATION"
-
-rm -rf "$TMP_DIR" && trap '' EXIT # cleanup
+log_github_install "$REPO" "$version" "$asset"
+tmp=$(download_file "https://github.com/$REPO/releases/download/$version/$asset")
+rm_exit "$tmp"
+apt_install "$tmp"
+rm_exit_cleanup "$tmp"
