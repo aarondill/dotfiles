14072b4f8186ffa48912f83b5f5baaa8096adaf6
aarondill <aaronsacks2006@gmail.com>
2023-08-05 03:57:11 -0500

    Update source/.config/git/config Add source/.config/git/alias-wrapper.sh

---
+++ source/private_dot_config/exact_git/executable_alias-wrapper.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# a wrapper script for git aliases.
+set -euC -o pipefail
+log() { printf '%s\n' "$@"; }
+err() { printf "${THIS:+$THIS: }%s\n" "$@" >&2; }
+abort() {
+  err "$1"
+  exit "${2:-1}"
+}
+
+command= && if [ -n "${1:-}" ]; then command="$1" && shift 1; fi
+case "$command" in
+shallowify)
+  git pull --depth "${1:-1}" "${@:2}" # default to depth 1
+  git gc --prune=all
+  ;;
+sha1) git rev-parse --short "${1:-HEAD}" ;; # default to HEAD
+hdiff) git diff "HEAD~${1:-}" ;;            # default to HEAD~
+mvbranch)
+  if [ "$#" -lt 2 ]; then abort "usage: mvbranch <from> <to>" 2; fi
+  git branch -m "$1" "$2"
+  git push origin ":$1"
+  git push --set-upstream origin "$2"
+  ;;
+alias)
+  if [ -z "${1:-}" ] || [ -z "${2:-}" ]; then abort 'usage: alias <alias> <definition>' 2; fi
+  git config --global "alias.$1" "$2"
+  ;;
+unalias)
+  if [ -z "${1:-}" ]; then abort "usage: unalias <alias>" 2; fi
+  git config --global --unset "alias.$1"
+  ;;
+'') abort "usage: alias-wrapper.sh <command> [args]..." 2 ;;
+*) THIS=alias-wrapper.sh abort "Unknown command '$command'" 2 ;;
+esac
