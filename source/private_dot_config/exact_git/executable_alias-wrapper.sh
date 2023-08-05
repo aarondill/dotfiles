8301c7c440cd588bfcbe5760e90dc402cc66ff65
aarondill <aaronsacks2006@gmail.com>
2023-08-05 04:20:44 -0500

    Update source/.config/git/alias-wrapper.sh

--- source/private_dot_config/exact_git/executable_alias-wrapper.sh
+++ source/private_dot_config/exact_git/executable_alias-wrapper.sh
@@ -32,7 +32,7 @@ unalias)
   ;;
 cleanup)
   err "Warning: this is a destructive operation."
-  sleep 2 # give chance to stop
+  sleep 1 # give chance to stop
   git reflog expire --expire=now
   git repack -d
   git gc --prune=now --aggressive
