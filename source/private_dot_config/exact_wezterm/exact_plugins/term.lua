78b73158546e1ebe2253510c29d029cc956eb184
aarondill <aaronsacks2006@gmail.com>
2023-08-07 04:41:46 -0500

    Update source/.config/wezterm/plugins/term.lua Update source/.config/wezterm/plugins/text.lua

--- source/private_dot_config/exact_wezterm/exact_plugins/term.lua
+++ source/private_dot_config/exact_wezterm/exact_plugins/term.lua
@@ -2,9 +2,9 @@
 local util = require("util")
 
 local root_terminfo = "/lib/terminfo/w/wezterm"
-local local_terminfo = os.getenv("HOME") .. "/.terminfo/w/wezterm"
+local local_terminfo = (os.getenv("HOME") or "") .. "/.terminfo/w/wezterm"
 if util.file_exists(root_terminfo) or util.file_exists(local_terminfo) then
-	return {
-		term = "wezterm",
-	}
+  return {
+    term = "wezterm",
+  }
 end
