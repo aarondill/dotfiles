4c108cba82f7719785cf8f07d64c3da348a6a7e6
aarondill <aaronsacks2006@gmail.com>
2023-08-09 10:41:26 -0500

    utils aren't included in the remove-sudo script.

--- source/.chezmoiscripts/20-post-install/run_once_after_00-remove-sudo.sh
+++ source/.chezmoiscripts/20-post-install/run_once_after_00-remove-sudo.sh
@@ -3,13 +3,13 @@
 set -euC -o pipefail
 
 if sudo sudo -V | grep -q -- --enable-admin-flag; then
-  cat <<'EOF' | sudo_cmd tee /etc/sudoers.d/disable_admin_file_in_home >/dev/null
+  cat <<'EOF' | sudo tee /etc/sudoers.d/disable_admin_file_in_home >/dev/null
 # Disable ~/.sudo_as_admin_successful file
 Defaults !admin_flag
 EOF
 fi
 
-sudo_cmd sed '/sudo hint/,/To run a command/s/cat <<-EOF/true <<-EOF/' /etc/bash.bashrc -i
+sudo sed '/sudo hint/,/To run a command/s/cat <<-EOF/true <<-EOF/' /etc/bash.bashrc -i
 
 if [ -f ~/.sudo_as_admin_successful ]; then
   rm ~/.sudo_as_admin_successful
