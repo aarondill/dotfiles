c76f70bfcfb4f04f5cc31d640f1d29f8472184a6
aarondill <aaronsacks2006@gmail.com>
2023-08-11 03:48:08 -0500

    check mcfly's version before running

--- source/.chezmoiscripts/10-install/run_onchange_after_30-mcfly.sh
+++ source/.chezmoiscripts/10-install/run_onchange_after_30-mcfly.sh
@@ -9,9 +9,13 @@ SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
 function install_mcfly() {
   local bindir='/usr/local/bin'
   local repo="cantino/mcfly"
+  current_version=$(run_own_shell mcfly --version | cut -d ' ' -f 2)
+  latest_version=$(get_latest_version_github "$repo")
+  if [ "${latest_version#v}" == "$current_version" ]; then abort "mcfly is already up to date" 0; fi
+
   sudo_cmd true
-  args=(sh -s -- --git "$repo" --to "$bindir" --force)
-  if [ -w "$bindir" ]; then
+  args=(sh -s -- --git "$repo" --to "$bindir" --tag "$latest_version" --force)
+  if ! [ -w "$bindir" ]; then
     args=(sudo_cmd "${args[@]}")
   fi
   download https://raw.githubusercontent.com/cantino/mcfly/master/ci/install.sh | "${args[@]}"
