535a10b8e450f54a69f37f68f00264986a1a0638
aarondill <aaronsacks2006@gmail.com>
2023-08-15 02:38:43 -0500

    Update source/.root/usr/local/bin/startup.sh

--- source/exact_dot_root/exact_usr/exact_local/exact_bin/executable_startup.sh
+++ source/exact_dot_root/exact_usr/exact_local/exact_bin/executable_startup.sh
@@ -4,7 +4,6 @@
 # startup.sh
 #
 # This shell program is for testing a startup like rc.local using systemd.
-# By Aaron Dill
 #
 ################################################################################
 
@@ -14,3 +13,6 @@
 
 # don't output to tty1.
 dmesg -D
+# Fix journal message about unknown keymap when using brightness keys (fn key is unknown)
+# 112 is usually unused
+sudo setkeycodes e05d 112
