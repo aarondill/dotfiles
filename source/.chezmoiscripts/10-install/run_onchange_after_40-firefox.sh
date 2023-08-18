d1def67115ad315c37d3d49f1d998a28610edca7
aarondill <aaronsacks2006@gmail.com>
2023-08-17 20:19:57 -0500

    install firefox and firejail

---
+++ source/.chezmoiscripts/10-install/run_onchange_after_40-firefox.sh
@@ -0,0 +1,38 @@
+#! /usr/bin/env bash
+set -euC -o pipefail
+export SHELLOPTS
+# Source utils
+SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
+# shellcheck source=../.utils.sh
+. "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
+
+# NOTE: Assumes firefox is in /usr/bin/firefox-esr
+# We can't extrapolate because we create a new symlink below
+firefox=/usr/bin/firefox
+firefox_esr=/usr/bin/firefox-esr
+firejail_links=("/usr/local/bin/firefox-esr" "/usr/local/bin/firefox")
+
+# if hasn't already run
+if ! [ -e "$firefox" ] && [ -x "$firefox_esr" ]; then
+  log "linking $firefox_esr to $firefox"
+  mklink "$firefox_esr" "$firefox"
+else
+  err "$firefox already linked to esr. Skipping linking."
+fi
+
+if has_cmd firejail; then
+  firejail=$(command -v firejail)
+  for c in "${firejail_links[@]}"; do
+    name=$(basename -- "$c")
+    if has_cmd "$name"; then
+      if ! [ -L "$c" ]; then
+        sudo_mkdir -p -- "$(basename -- "$c")"
+        mklink "$firejail" "$c"
+      else
+        err "Skipping link $c. Link already exists."
+      fi
+    fi
+  done
+else
+  abort 'firejail not found. Skipping firejail links.' 0
+fi
