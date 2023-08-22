12808e03b3e4309ea65f1cd709939de2d05c34ae
aarondill <aaronsacks2006@gmail.com>
2023-08-22 13:08:48 -0500

    Add source/code/Java/template/ClassName.java Add source/code/Java/template/ClassName.test.sh

---
+++ source/code/exact_Java/exact_template/ClassName.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * public class ClassName
+ *
+ * <pre>
+ * Author:  Aaron Dill
+ * Date:    $DATE
+ * Course:  Computer Science I AP
+ * Period:  3rd
+ * </pre>
+ *
+ * Summary of file:
+ *
+ */
+public class ClassName {
+  public static void main(String... args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println(scan.nextLine());
+    System.out.println("Hello World!");
+    scan.close();
+  }
+}
