02a18587ac35693fb48e05ef8722075aec35697d
aarondill <aaronsacks2006@gmail.com>
2023-09-10 03:20:14 -0500

    add an add_groups script to ensure needed groups are assigned to the user

---
+++ source/.chezmoiscripts/20-post-install/run_once_after_00_add_groups.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+set -euC -o pipefail
+export SHELLOPTS
+# Source utils
+SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
+# shellcheck source=../.utils.sh
+. "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
+
+groups=(adm cdrom video plugdev input lpadmin vboxusers libvirt loadkeys power)
+user="$(id -un)"
+
+if [ -z "$user" ] || [ "$user" == 'root' ]; then abort "Can't set groups on user '$user'" 1; fi
+if has_cmd getent; then
+  function group_exists() { getent group -- "$1" &>/dev/null; }
+elif has_cmd perl; then
+  function group_exists() { perl -e 'my $gid = getgrnam($ARGV[0]); exit 1 if !defined($gid)' -- "$1"; }
+else
+  abort 'perl or getent is required to add groups' 1
+fi
+for group in "${groups[@]}"; do
+  group_exists "$group" || continue # group doesn't exist, skip it
+  usermod -a -G "$group" -- "$user" # add to the group
+done
