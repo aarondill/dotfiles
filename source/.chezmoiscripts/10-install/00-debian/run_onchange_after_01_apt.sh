13b4ed2a5df221a4ad0a9b63c7ce7e0165717d09
aarondill <aaronsacks2006@gmail.com>
2023-09-24 20:36:56 -0500

    fix abort on no graphical packages

--- source/.chezmoiscripts/10-install/00-debian/run_onchange_after_01_apt.sh
+++ source/.chezmoiscripts/10-install/00-debian/run_onchange_after_01_apt.sh
@@ -75,7 +75,7 @@ function install_graphical_packages() {
   # If gnome is not installed, ask confirmation, else just install
   if [ -n "$GNOME" ]; then graphical_packages+=("${GNOME_PACKAGES[@]}"); fi
   if ! has_cmd X && ! has_cmd Xorg && [ -z "$GNOME" ]; then
-    confirm "Xorg is not installed, would you still like to install gui applications?"
+    confirm "Xorg is not installed, would you still like to install gui applications?" || return 0
   fi
   if has_cmd virt-manager || confirm "Would you like to install qemu-desktop and other virtual machine packages?"; then
     graphical_packages=("${graphical_packages[@]}" "${VIRTUAL_MACHINE_PACKAGES[@]}")
