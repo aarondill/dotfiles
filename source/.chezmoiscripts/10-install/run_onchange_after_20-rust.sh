8f24d5578c8fecc27a24909617bf6d173e2c1e8e
aarondill <aaronsacks2006@gmail.com>
2023-10-03 21:55:28 -0500

    use vendored deps in cargo-update

--- source/.chezmoiscripts/10-install/run_onchange_after_20-rust.sh
+++ source/.chezmoiscripts/10-install/run_onchange_after_20-rust.sh
@@ -24,10 +24,7 @@ else
 fi
 
 if has_cmd cargo && ! has_cmd cargo-install-update; then
-  if has_apt; then # dep install
-    apt_install libssl-dev
-  elif has_pacman; then
-    pacman_install openssl
-  fi
-  cargo install cargo-update # install update crate for update script
+  # install update crate for update script
+  # Note: deps are vendored with all-features
+  cargo install --all-features cargo-update
 fi
