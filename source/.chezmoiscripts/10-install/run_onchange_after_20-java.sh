41673c540bb3b75b5f13754807888a60ffd927ae
aarondill <aaronsacks2006@gmail.com>
2023-11-14 00:53:42 -0600

    improve java install script
    now allows for multiple versions to be installed

--- source/.chezmoiscripts/10-install/run_onchange_after_20-java.sh
+++ source/.chezmoiscripts/10-install/run_onchange_after_20-java.sh
@@ -6,20 +6,31 @@ SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
 . "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
 
 destination="/usr/lib/jvm" # Fixed location
-version=20.0.2
-url=https://download.java.net/java/GA/jdk20.0.2/6e380f22cbe7469fa75fb448bd903d8e/9/GPL/openjdk-20.0.2_linux-x64_bin.tar.gz
+function install_java() {
+  local tmp version="$1" url="$2"
+  if [ -x "$destination/jdk-$version/bin/java" ]; then
+    err "jdk-$version is already installed."
+    return 0
+  fi
+  log "downloading jdk-$version (fixed version)"
+  tmp=$(download_file "$url")
+  rm_exit "$tmp"
+  # output to destination
+  sudo_cmd mkdir -p "$destination"
+  sudo_cmd tar -xz -C "$destination" -f "$tmp"
+  rm_exit_cleanup "$tmp"
+}
+base_url="https://download.java.net/java/GA" update_version="20.0.2"
+declare -A versions=(
+  [20.0.2]="$base_url/jdk20.0.2/6e380f22cbe7469fa75fb448bd903d8e/9/GPL/openjdk-20.0.2_linux-x64_bin.tar.gz"
+)
 
-if [ -x "$destination/jdk-$version/bin/java" ]; then
-  abort "jdk-$version is already installed." 0
+if [ -z "${versions[$update_version]:-}" ]; then
+  abort "Ensure update_version is in the installed versions list!" 2
 fi
+for v in "${!versions[@]}"; do
+  install_java "$v" "${versions[$v]}"
+done
 
-log "downloading jdk-$version (fixed version)"
-
-tmp=$(download_file "$url")
-rm_exit "$tmp"
-# output to destination
-sudo_cmd mkdir -p "$destination"
-sudo_cmd tar -xz -C "$destination" -f "$tmp"
-rm_exit_cleanup "$tmp"
 # This is my own script! should be in ~/.local/bin/update-java!
-sudo_cmd ~/.local/bin/update-java --quiet --yes "$destination/jdk-$version"
+sudo_cmd ~/.local/bin/update-java --quiet --yes "$destination/jdk-$update_version"
