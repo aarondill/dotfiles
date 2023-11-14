b79e3307d3df537d7e77d827d64a60ef951fd402
aarondill <aaronsacks2006@gmail.com>
2023-11-14 01:13:54 -0600

    log unpack vs download

--- source/.chezmoiscripts/10-install/run_onchange_after_20-java.sh
+++ source/.chezmoiscripts/10-install/run_onchange_after_20-java.sh
@@ -16,6 +16,7 @@ function install_java() {
   tmp=$(download_file "$url")
   rm_exit "$tmp"
   # output to destination
+  log "unpacking jdk-$version"
   sudo_cmd mkdir -p "$destination"
   sudo_cmd tar -xz -C "$destination" -f "$tmp"
   rm_exit_cleanup "$tmp"
