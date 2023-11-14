33a6815004b5d8b5aa0bf801bc1b9e0d1007c065
aarondill <aaronsacks2006@gmail.com>
2023-11-14 01:04:59 -0600

    add other versions to java

--- source/.chezmoiscripts/10-install/run_onchange_after_20-java.sh
+++ source/.chezmoiscripts/10-install/run_onchange_after_20-java.sh
@@ -9,10 +9,10 @@ destination="/usr/lib/jvm" # Fixed location
 function install_java() {
   local tmp version="$1" url="$2"
   if [ -x "$destination/jdk-$version/bin/java" ]; then
-    err "jdk-$version is already installed."
+    log "jdk-$version is already installed."
     return 0
   fi
-  log "downloading jdk-$version (fixed version)"
+  log "downloading jdk-$version"
   tmp=$(download_file "$url")
   rm_exit "$tmp"
   # output to destination
@@ -20,9 +20,12 @@ function install_java() {
   sudo_cmd tar -xz -C "$destination" -f "$tmp"
   rm_exit_cleanup "$tmp"
 }
-base_url="https://download.java.net/java/GA" update_version="20.0.2"
+base_url="https://download.java.net/java" update_version="21.0.1"
 declare -A versions=(
-  [20.0.2]="$base_url/jdk20.0.2/6e380f22cbe7469fa75fb448bd903d8e/9/GPL/openjdk-20.0.2_linux-x64_bin.tar.gz"
+  [17.0.2]="$base_url/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_linux-x64_bin.tar.gz" # Don't use this version! It's outdated!
+  # [20.0.2]="$base_url/GA/jdk20.0.2/6e380f22cbe7469fa75fb448bd903d8e/9/GPL/openjdk-20.0.2_linux-x64_bin.tar.gz"
+  [21.0.1]="$base_url/GA/jdk21.0.1/415e3f918a1f4062a0074a2794853d0d/12/GPL/openjdk-21.0.1_linux-x64_bin.tar.gz"
+  [22]="$base_url/early_access/jdk22/23/GPL/openjdk-22-ea+23_linux-x64_bin.tar.gz"
 )
 
 if [ -z "${versions[$update_version]:-}" ]; then
