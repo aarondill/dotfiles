c124fe864634f1ffca9fb127fd919d79393fb6f8
aarondill <aaronsacks2006@gmail.com>
2023-12-08 15:19:32 -0600

    Update source/code/java/UIL/template/$DIRNAME.java Update source/.local/bin/new

--- source/code/java/exact_UIL/exact_template/$DIRNAME.java
+++ source/code/java/exact_UIL/exact_template/$DIRNAME.java
@@ -1,15 +1,14 @@
+import java.util.Scanner;
+
 public class $BASENAME {
   public static void main(String... args) {
-    // All paths are relative to the source root unless specified otherwise.
-    System.out.println("FILEPATH: ${FILEPATH}"); // This file's path
-    System.out.println("REALPATH: ${REALPATH}"); // This file's absolute path
-    System.out.println("FILENAME: ${FILENAME}"); // This file's name
-    System.out.println("DIRNAME: ${DIRNAME}"); // This file's directory name
-    System.out.println("DATE: ${DATE}"); // The current date mm/dd/yyyy
-    System.out.println("TIME: ${TIME}"); // The current time hh:mm 24hr
-    System.out.println("EXTENSION: ${EXTENSION}"); // This file's extension (last!)
-    System.out.println("BASENAME: ${BASENAME}"); // This file's name without extension
-    System.out.println("ROOT: ${ROOT}"); // The name of root (project)
-    System.out.println("Hello World!");
+    try (Scanner dataScanner = new Scanner($BASENAME.class.getResourceAsStream("./${dirname}.dat"))) {
+      int dataCount = dataScanner.nextInt();
+      dataScanner.nextLine();
+      for (int i = 0; i < dataCount; i++) {
+        // Do Some Stuff
+      }
+    }
+
   }
 }
