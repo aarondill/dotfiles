1c2e9232d9ae457c2be5c443ed450eaab7b764a9
aarondill <aaronsacks2006@gmail.com>
2024-02-08 00:47:37 -0600

    feat: update java version numbers

--- source/.chezmoiscripts/10-install/run_onchange_after_20-java.sh
+++ source/.chezmoiscripts/10-install/run_onchange_after_20-java.sh
@@ -21,12 +21,11 @@ function install_java() {
   sudo_cmd tar -xz -C "$destination" -f "$tmp"
   rm_exit_cleanup "$tmp"
 }
-base_url="https://download.java.net/java" update_version="21.0.1"
+base_url="https://download.java.net/java" update_version="21.0.2"
 declare -A versions=(
   [17.0.2]="$base_url/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_linux-x64_bin.tar.gz" # Don't use this version! It's outdated!
-  # [20.0.2]="$base_url/GA/jdk20.0.2/6e380f22cbe7469fa75fb448bd903d8e/9/GPL/openjdk-20.0.2_linux-x64_bin.tar.gz"
-  [21.0.1]="$base_url/GA/jdk21.0.1/415e3f918a1f4062a0074a2794853d0d/12/GPL/openjdk-21.0.1_linux-x64_bin.tar.gz"
-  [22]="$base_url/early_access/jdk22/23/GPL/openjdk-22-ea+23_linux-x64_bin.tar.gz"
+  [21.0.2]="$base_url/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_linux-x64_bin.tar.gz"
+  [23]="$base_url/early_access/jdk23/8/GPL/openjdk-23-ea+8_linux-x64_bin.tar.gz"
 )
 
 if [ -z "${versions[$update_version]:-}" ]; then
