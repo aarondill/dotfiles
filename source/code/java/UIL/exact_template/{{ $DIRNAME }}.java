77aa1b32931d408e32d46473110a0e16851445b8
aarondill <aaronsacks2006@gmail.com>
2024-02-12 05:57:54 -0600

    Update source/code/java/UIL/template/{{ $DIRNAME }}.java

--- source/code/java/UIL/exact_template/{{ $DIRNAME }}.java
+++ source/code/java/UIL/exact_template/{{ $DIRNAME }}.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class {{ $BASENAME }} {
   public static void main(String... args) {
-    try (Scanner dataScanner = new Scanner({{ $BASENAME }}.class.getResourceAsStream("./{{ ${CLASS_NAME,} }}.dat"))) {
+    try (Scanner dataScanner = new Scanner({{ $BASENAME }}.class.getResourceAsStream("./{{ ${BASENAME,} }}.dat"))) {
       int dataCount = dataScanner.nextInt();
       dataScanner.nextLine();
       for (int i = 0; i < dataCount; i++) {
