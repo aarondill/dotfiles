dc726f00a3eece3af807501a6f058b6028e3beb7
aarondill <aaronsacks2006@gmail.com>
2024-02-15 03:29:01 -0600

    Add autoupdate Update source/.local/bin/chezmoi/common.sh

--- source/dot_local/exact_bin/exact_chezmoi/common.sh
+++ source/dot_local/exact_bin/exact_chezmoi/common.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
-# The file containing files to auto update
-export CONFIG_FILES=~/.config/chezmoi/autoupdate
-# A stored copy of the crontab
-export CRONTAB_FILE=~/.config/crontab
-# A stored copy of the dconf settings
-export DCONF_BACKUP=~/.config/dconf/dconf-backup.ini
-# The directory under $source where files from / are stored
-export ROOT_FOLDER=~/.root
-
 check_dependencies chezmoi
+# This is the target for chezmoi. This is usually $HOME, but could be different
 target=$(chezmoi target-path) || abort "Could not find target-path"
 [ -d "$target" ] || abort "target-path is not a directory: ${target}"
+# This is the source of the chezmoi repository.
+# If .chezmoiroot is present, it will not be the root of the git repository.
 source=$(chezmoi source-path) || abort "Could not find source-path"
 [ -d "$source" ] || abort "source-path is not a directory: ${source}"
+# This is the git repository directory (root).
+# This may match $source, or it may be different if .chezmoiroot is present.
+chezmoi_dir=$(chezmoi execute-template '{{.chezmoi.workingTree}}')
+[ -d "$chezmoi_dir" ] || abort "chezmoi.workingTree is not a directory: ${chezmoi_dir}"
+
+# The file containing files to auto update
+export CONFIG_FILES="$chezmoi_dir/autoupdate"
+# A stored copy of the crontab
+# This file should be in a location that is backed up by chezmoi
+export CRONTAB_FILE=~/.config/crontab
+# A stored copy of the dconf settings -- Not directly backed up by chezmoi
+export DCONF_BACKUP=~/.config/dconf/dconf-backup.ini
+# The directory under $source where files from / are stored
+# This directory should be in a location that is backed up by chezmoi
+export ROOT_FOLDER=~/.root
