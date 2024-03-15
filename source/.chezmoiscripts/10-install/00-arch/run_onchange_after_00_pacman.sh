a5eb7eafdf7d013e0cf866346e66fad7dae35562
aarondill <aaronsacks2006@gmail.com>
2024-03-15 02:07:33 -0500

    add xdg-user-dirs to pacan script

--- source/.chezmoiscripts/10-install/00-arch/run_onchange_after_00_pacman.sh
+++ source/.chezmoiscripts/10-install/00-arch/run_onchange_after_00_pacman.sh
@@ -16,7 +16,7 @@ PACKAGES=(
   openvpn p7zip rsync shfmt tlp trash-cli tree util-linux
   zip unzip zoxide speedtest-cli ripgrep hexedit fuse2
   luajit python-pip base-devel wezterm bat bash-completion
-  git-delta vivaldi
+  git-delta vivaldi xdg-user-dirs
   # fonts
   ttf-nerd-fonts-symbols-mono noto-fonts-cjk noto-fonts-extra ttf-liberation
   # icon fonts
