204af6da5eae65a9a2cb3452b6c8b798bbba576c
aarondill <aaronsacks2006@gmail.com>
2024-04-20 02:37:07 -0500

    fix: use pacman to install eza
    I no longer need/want to track git. The pacman releases track the git releases, so skip compilation.

--- source/.chezmoiscripts/10-install/run_onchange_after_30-eza.sh
+++ source/.chezmoiscripts/10-install/run_onchange_after_30-eza.sh
@@ -5,14 +5,17 @@ SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
 # shellcheck source=../.utils.sh
 . "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
 
-REPO_URL='https://github.com/eza-community/eza'
-
 install_eza() {
-  cargo install --all-features --git "$REPO_URL"
+  # eza is in extra repo on arch
+  if has_pacman; then
+    pacman_install eza
+  elif has_cmd cargo; then
+    # REPO_URL='https://github.com/eza-community/eza'
+    # cargo install --all-features --git "$REPO_URL"
+    cargo install --all-features eza
+  else
+    err "Neither pacman nor cargo are available! Can't install eza!"
+  fi
 }
 
-# if has_pacman; then # eza is in extra repo on arch
-#   log_and_run "Installing eza using pacman" pacman -S eza
-if has_cmd cargo; then # only with cargo installed
-  log_and_run "Installing eza using cargo" install_eza
-fi
+log_and_run "Installing eza" install_eza
