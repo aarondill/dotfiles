9e510b0b0d9f677ba79ef25d3fcc5fc1cd7bd76b
Aaron Dill <aaronsacks2006@gmail.com>
2024-07-31 01:33:26 -0500

    fix: can't return from script

--- source/.chezmoiscripts/10-install/run_onchange_after_30-eza.sh
+++ source/.chezmoiscripts/10-install/run_onchange_after_30-eza.sh
@@ -4,6 +4,7 @@ set -euC -o pipefail
 SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
 # shellcheck source=../.utils.sh
 . "$SOURCE_DIR/.chezmoiscripts/.utils.sh"
+! has_cmd eza || exit 0
 
 install_eza() {
   # REPO_URL='https://github.com/eza-community/eza'
@@ -11,12 +12,8 @@ install_eza() {
   cargo install --all-features eza
 }
 
-if has_cmd eza; then
-  log "eza is already installed"
-  return 0
-fi
 if ! has_cmd cargo; then
   err "cargo isn't available! Can't install eza!"
-  return 1
+  exit 1
 fi
 log_and_run "Installing eza" install_eza
