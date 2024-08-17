9d95bbba2a433da6c673a736e34da9741d47b5d1
Aaron Dill <aaronsacks2006@gmail.com>
2024-08-17 06:29:21 -0500

    fix: i don't need xdg-desktop-portal-gnome

--- source/.chezmoiscripts/10-install/00-arch/run_onchange_after_00_pacman.sh
+++ source/.chezmoiscripts/10-install/00-arch/run_onchange_after_00_pacman.sh
@@ -32,11 +32,9 @@ GRAPHICAL_PACKAGES=(
   arandr dconf-editor eog evince file-roller firefox firejail gimp
   gnome-calculator gnome-calendar gnome-font-viewer gnome-power-manager
   gnome-system-monitor gparted gucharmap hplip libreoffice-fresh mpv mpv-mpris
-  nautilus onboard pavucontrol qtqr rhythmbox screenkey seahorse
-  simple-scan spotify-launcher sqlitebrowser thunderbird transmission-gtk
-  wmctrl xclip xdg-desktop-portal-gnome xdg-desktop-portal-gtk xdg-utils
-  xdotool xorg-xinit
-  zeal
+  nautilus onboard pavucontrol qtqr rhythmbox screenkey seahorse simple-scan
+  spotify-launcher sqlitebrowser thunderbird transmission-gtk wmctrl xclip
+  xdg-desktop-portal-gtk xdg-utils xdotool xorg-xinit zeal
 )
 GNOME_PACKAGES=(gnome-shell-extension-manager gnome-tweaks gnome-software gnome-software-plugin-flatpak)
 
