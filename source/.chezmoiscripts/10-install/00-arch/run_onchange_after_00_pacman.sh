0616f4532a93ace07b6a26b44ce1dbccd8246c96
Aaron Dill <aaronsacks2006@gmail.com>
2025-01-11 01:51:13 -0600

    fix: pacman packages

--- source/.chezmoiscripts/10-install/00-arch/run_onchange_after_00_pacman.sh
+++ source/.chezmoiscripts/10-install/00-arch/run_onchange_after_00_pacman.sh
@@ -16,8 +16,8 @@ PACKAGES=(
   eza fastfetch ffmpeg fuse2 gdb git git-delta github-cli grep hexedit
   imagemagick inetutils inotify-tools jq lazygit less libpulse lsof lua luajit
   make mdcat moreutils neovim net-tools networkmanager-openvpn nnn noto-fonts-cjk
-  noto-fonts-emoji noto-fonts-extra openssh openvpn os-prober p7zip
-  pacman-contrib pacutils pigz pkgfile plocate posix progress pulseaudio pv
+  noto-fonts-emoji noto-fonts-extra openssh openvpn os-prober 7zip
+  pacman-contrib pacutils pigz pkgfile plocate posix progress pipewire-pulse pv
   python-pip reflector renameutils ripgrep ripgrep-all rlwrap rsync shfmt silicon
   speedtest-cli strace sudo tlp trash-cli tree ttf-font-awesome ttf-liberation
   ttf-nerd-fonts-symbols-mono unzip util-linux vivaldi wezterm wget xdg-user-dirs
