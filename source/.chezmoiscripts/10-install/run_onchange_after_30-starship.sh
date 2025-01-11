9aa3b7e58523d2a7153f491f23054e24c5a3ede4
Aaron Dill <aaronsacks2006@gmail.com>
2025-01-11 02:12:37 -0600

    fix: manage starship from pacman repos

--- source/.chezmoiscripts/10-install/run_onchange_after_30-starship.sh
+++ source/.chezmoiscripts/10-install/run_onchange_after_30-starship.sh
@@ -7,12 +7,16 @@ SOURCE_DIR="${CHEZMOI_SOURCE_DIR:-"$(chezmoi source-path)"}"
 INSTALL_URL=https://starship.rs/install.sh
 INSTALL_DIR=/usr/local/bin
 
-# Use the install script! We love this! :)
-install_script="$(download "$INSTALL_URL")"
-# If ends in print_install, remove it. It's annoying. Be as safe as possible.
-if [ "print_install" = "$(tail -n1 <<<"$install_script")" ]; then
-  install_script=$(head -n-1 <<<"$install_script")
+if has_pacman; then
+  pacman_install starship
+else
+  # Use the install script! We love this! :)
+  install_script="$(download "$INSTALL_URL")"
+  # If ends in print_install, remove it. It's annoying. Be as safe as possible.
+  if [ "print_install" = "$(tail -n1 <<<"$install_script")" ]; then
+    install_script=$(head -n-1 <<<"$install_script")
+  fi
+  log "Installing starship from $INSTALL_URL"
+  sudo_writable "$INSTALL_DIR/starship" sh -c "$install_script" -- -y -b "$INSTALL_DIR"
 fi
-log "Installing starship from $INSTALL_URL"
-sudo_writable "$INSTALL_DIR/starship" sh -c "$install_script" -- -y -b "$INSTALL_DIR"
 success
