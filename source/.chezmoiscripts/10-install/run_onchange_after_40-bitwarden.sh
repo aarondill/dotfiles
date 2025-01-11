9ad3681e0ad4ff14bbbc3d4d4ab57a9dcb8dcc96
Aaron Dill <aaronsacks2006@gmail.com>
2025-01-11 01:27:41 -0600

    Update source/.chezmoiscripts/10-install/40-bitwarden.sh Remove source/.local/share/applications/bitwarden.desktop

--- source/.chezmoiscripts/10-install/run_onchange_after_40-bitwarden.sh
+++ source/.chezmoiscripts/10-install/run_onchange_after_40-bitwarden.sh
@@ -10,28 +10,8 @@ function install_bitwarden_desktop() {
   download_file 'https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=appimage' "$DESTINATION" "+x"
 }
 
-function install_bitwarden_cli() (
-  local DESTINATION=/usr/local/bin/bw temp_dir
-  # shellcheck disable=SC2206 # ik it splits. that's the point
-  local zip_file
-  zip_file=$(download_file 'https://vault.bitwarden.com/download/?app=cli&platform=linux')
-  rm_exit "$zip_file"
-  temp_dir=$(mktemp -d)
-  rm_exit "$temp_dir"
-  unzip -qq "$zip_file" -d "$temp_dir"
-  sudo_cmd mkdir -p "$(dirname "$DESTINATION")"
-  sudo_cmd install "$temp_dir/bw" "$DESTINATION"
-  sudo_cmd chmod +x "$DESTINATION"
-  rm_exit_cleanup "$temp_dir"
-)
-
-## Ignore if already installed, updates itself -- not when permissions don't work for it.
-# if ! has_cmd bitwarden; then
-log_and_run "Installing bitwarden desktop" install_bitwarden_desktop
-# fi
-
-if ! has_cmd unzip; then
-  abort "unzip is required to install bitwarden cli" 2
+if has_pacman; then
+  pacman_install 'bitwarden'
+else
+  log_and_run "Installing bitwarden desktop" install_bitwarden_desktop
 fi
-
-log_and_run "Installing bitwarden CLI" install_bitwarden_cli
