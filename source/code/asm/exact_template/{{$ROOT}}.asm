fabd9adde6af9b1bf28ca6dd2754c797a5109fa3
Aaron Dill <aaronsacks2006@gmail.com>
2026-08-26 13:37:07 -0500

    Add code/asm/template/SysCalls.asm Update code/asm/template/{{$ROOT}}.asm

--- source/code/asm/exact_template/{{$ROOT}}.asm
+++ source/code/asm/exact_template/{{$ROOT}}.asm
@@ -2,15 +2,17 @@
 # Date: {{$DATE}}
 # Description: ______________________________
 
+.include "SysCalls.asm"
+
 .data
 # Data segment: constant and variable definitions go here
 msg: .asciiz "Hello, World!\n"
 
 .text
 # Text segment: assembly instructions go here
-    li $v0, 4 # print string
+    li $v0, SysPrintString # print string
     la $a0, msg
     syscall
-    li $v0, 10 # exit
+    li $v0, SysExit # exit
     syscall
 
